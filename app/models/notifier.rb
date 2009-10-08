72d1fe45723ee9a586679fd2b62481389ca99fbc
Carlos A. da Silva <carlosantoniodasilva@gmail.com>
2009-10-08 11:40:53 -0300

    Refactoring notifier and adding locale translation.

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,21 +1,28 @@
 class Notifier < ::ActionMailer::Base
   cattr_accessor :sender
 
+  # Deliver confirmation instructions when the user is created or confirmation
+  # is manually requested
+  #
   def confirmation_instructions(record)
-    from         self.class.sender
-    recipients   record.email
-    subject      I18n.t(:confirmation_instructions, :scope => [:devise, :notifier], :default => 'Confirmation instructions')
-    sent_on      Time.now
-    content_type 'text/html'
-    body         record.class.name.downcase.to_sym => record
+    subject I18n.t(:confirmation_instructions, :scope => [:devise, :notifier], :default => 'Confirmation instructions')
+    setup_mail(record)
   end
 
+  # Deliver reset password instructions when manually requested
+  #
   def reset_password_instructions(record)
-    from         self.class.sender
-    recipients   record.email
-    subject      I18n.t(:reset_password_instructions, :scope => [:devise, :notifier], :default => 'Reset password instructions')
-    sent_on      Time.now
-    content_type 'text/html'
-    body         record.class.name.downcase.to_sym => record
+    subject I18n.t(:reset_password_instructions, :scope => [:devise, :notifier], :default => 'Reset password instructions')
+    setup_mail(record)
   end
+
+  private
+
+    def setup_mail(record)
+      from         self.class.sender
+      recipients   record.email
+      sent_on      Time.now
+      content_type 'text/html'
+      body         record.class.name.downcase.to_sym => record
+    end
 end

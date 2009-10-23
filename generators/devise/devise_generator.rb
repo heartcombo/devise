385dba551e6bec3c419b975bb7eeb59a4d78ec6a
Carlos A. da Silva <carlosantoniodasilva@gmail.com>
2009-10-23 10:13:23 -0200

    Adding simple devise generator to create a model, migration and route.

---
+++ generators/devise/devise_generator.rb
@@ -0,0 +1,25 @@
+require File.expand_path(File.dirname(__FILE__) + "/lib/route_devise.rb")
+
+class DeviseGenerator < Rails::Generator::NamedBase
+
+  def manifest
+    record do |m|
+      # Check for class naming collisions.
+      m.class_collisions(class_name)
+
+      # Model
+      m.directory(File.join('app', 'models', class_path))
+      m.template 'model.rb', File.join('app', 'models', "#{file_path}.rb")
+
+      # Migration
+      m.migration_template 'migration.rb', 'db/migrate', :migration_file_name => "devise_create_#{table_name}"
+
+      # Routing
+      m.route_devise table_name
+
+      # Readme
+      m.readme "README"
+    end
+  end
+
+end
---
+++ generators/devise/templates/migration.rb
@@ -0,0 +1,20 @@
+class DeviseCreate<%= table_name.camelize %> < ActiveRecord::Migration
+  def self.up
+    create_table(:<%= table_name %>) do |t|
+      t.authenticable
+      t.confirmable
+      t.recoverable
+      t.rememberable
+
+      t.timestamps
+    end
+
+    add_index :<%= table_name %>, :email,                :unique => true
+    add_index :<%= table_name %>, :confirmation_token,   :unique => true
+    add_index :<%= table_name %>, :reset_password_token, :unique => true
+  end
+
+  def self.down
+    drop_table :<%= table_name %>
+  end
+end

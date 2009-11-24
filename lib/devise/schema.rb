327243867164e195c5ec17ea21b0f4844afc6c9e
José Valim <jose.valim@gmail.com>
2009-11-24 15:46:54 -0200

    Fix small typo on docs.

--- lib/devise/schema.rb
+++ lib/devise/schema.rb
@@ -42,7 +42,7 @@ module Devise
     end
 
     # Creates sign_in_count, current_sign_in_at, last_sign_in_at,
-    # current_sign_in_ip, last_sign_in_in.
+    # current_sign_in_ip, last_sign_in_ip.
     def trackable
       apply_schema :sign_in_count,      Integer
       apply_schema :current_sign_in_at, DateTime

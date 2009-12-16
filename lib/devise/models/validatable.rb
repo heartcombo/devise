436300928b75668d3dfcef58850a2492ad06acd4
José Valim <jose.valim@gmail.com>
2009-12-16 01:28:43 +0100

    Send :scope to the proper validation.

--- lib/devise/models/validatable.rb
+++ lib/devise/models/validatable.rb
@@ -19,9 +19,8 @@ module Devise
 
         base.class_eval do
           validates_presence_of   :email
-          validates_uniqueness_of :email, :allow_blank => true
-          validates_format_of     :email, :with => EMAIL_REGEX, :allow_blank => true,
-                                          :scope => authentication_keys[1..-1]
+          validates_uniqueness_of :email, :scope => authentication_keys[1..-1], :allow_blank => true
+          validates_format_of     :email, :with  => EMAIL_REGEX, :allow_blank => true
 
           with_options :if => :password_required? do |v|
             v.validates_presence_of     :password

3435c537251596d57cf09264fcde22c819eb908a
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2010-02-12 13:00:58 -0200

    Fix typo: autoload Clearance encryptor and not Authlogic one.

--- lib/devise.rb
+++ lib/devise.rb
@@ -13,7 +13,7 @@ module Devise
     autoload :Base, 'devise/encryptors/base'
     autoload :Bcrypt, 'devise/encryptors/bcrypt'
     autoload :AuthlogicSha512, 'devise/encryptors/authlogic_sha512'
-    autoload :AuthlogicSha1, 'devise/encryptors/authlogic_sha1'
+    autoload :ClearanceSha1, 'devise/encryptors/clearance_sha1'
     autoload :RestfulAuthenticationSha1, 'devise/encryptors/restful_authentication_sha1'
     autoload :Sha512, 'devise/encryptors/sha512'
     autoload :Sha1, 'devise/encryptors/sha1'

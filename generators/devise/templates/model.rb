38bfe3f990acb0cbb56fdc5a11a0f84c5f321505
José Valim <jose.valim@gmail.com>
2010-02-17 10:53:11 +0100

    First generator for Rails 3.

--- generators/devise/templates/model.rb
+++
@@ -1,9 +0,0 @@
-class <%= class_name %> < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :http_authenticatable, :token_authenticatable, :lockable, :timeoutable and :activatable
-  devise :registerable, :authenticatable, :confirmable, :recoverable,
-         :rememberable, :trackable, :validatable
-
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation
-end
---
+++ lib/generators/devise/devise_generator.rb
@@ -0,0 +1,54 @@
+require 'rails/generators/migration'
+
+class DeviseGenerator < Rails::Generators::NamedBase
+  include Rails::Generators::Migration
+
+  def self.source_root
+    @_devise_source_root ||= File.expand_path("../templates", __FILE__)
+  end
+
+  def self.orm_has_migration?
+    Rails::Generators.options[:rails][:orm] == :active_record
+  end
+
+  def self.next_migration_number(path)
+    Time.now.utc.strftime("%Y%m%d%H%M%S")
+  end
+
+  class_option :migration, :type => :boolean, :default => orm_has_migration?
+
+  def invoke_orm_model
+    if File.exists?(File.join(destination_root, model_path))
+      say "* Model already exists. Adding Devise behavior."
+    else
+      invoke "model", [name], :migration => false
+    end
+  end
+
+  def inject_devise_config_into_model
+    inject_into_class model_path, class_name, <<-CONTENT
+  # Include default devise modules. Others available are:
+  # :http_authenticatable, :token_authenticatable, :lockable, :timeoutable and :activatable
+  devise :registerable, :authenticatable, :confirmable, :recoverable,
+         :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation
+CONTENT
+  end
+
+  def copy_migration_template
+    return unless options.migration?
+    migration_template "migration.rb", "db/migrate/devise_create_#{table_name}"
+  end
+
+  def add_devise_routes
+    route "devise_for :#{table_name}"
+  end
+
+  protected
+
+    def model_path
+      @model_path ||= File.join("app", "models", "#{file_path}.rb")
+    end
+end

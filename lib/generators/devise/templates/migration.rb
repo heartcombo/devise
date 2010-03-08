1a740774e3db8816e690e7e6a528b4d612191c5b
Ørjan Blom <blom@blom.tv>
2010-03-08 03:56:26 +0100

    use Devise.encryptor setting when creating the migration
    Signed-off-by: José Valim <jose.valim@gmail.com>

--- lib/generators/devise/templates/migration.rb
+++ lib/generators/devise/templates/migration.rb
@@ -1,7 +1,7 @@
 class DeviseCreate<%= table_name.camelize %> < ActiveRecord::Migration
   def self.up
     create_table(:<%= table_name %>) do |t|
-      t.authenticatable :encryptor => :sha1, :null => false
+      t.authenticatable :encryptor => :<%= Devise.encryptor %>, :null => false
       t.confirmable
       t.recoverable
       t.rememberable

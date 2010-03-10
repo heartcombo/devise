ed3a460badcddbb963f8819ea442ee1dfd3b0169
José Valim <jose.valim@gmail.com>
2010-03-10 16:53:09 +0100

    Allow to give a scope when copying views.

--- lib/generators/devise_views/devise_views_generator.rb
+++ lib/generators/devise_views/devise_views_generator.rb
@@ -1,15 +1,14 @@
 class DeviseViewsGenerator < Rails::Generators::Base
   desc "Copies all Devise views to your application."
 
+  argument :scope, :required => false, :default => nil,
+                   :desc => "The scope to copy views to"
+
   def self.source_root
     @_devise_source_root ||= File.expand_path("../../../../app/views", __FILE__)
   end
 
   def copy_views
-    directory "devise", "app/views/devise"
-  end
-
-  def say_restart_server
-    say "Views copied. Please restart your server."
+    directory "devise", "app/views/devise/#{scope}"
   end
 end

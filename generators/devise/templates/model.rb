c29b6ca4ea66816716cb893baa4b29e8c336c729
José Valim <jose.valim@gmail.com>
2010-05-03 00:08:48 +0200

    Confirmable is not default anymore. This provides a better bootstrap experience.

--- generators/devise/templates/model.rb
+++ generators/devise/templates/model.rb
@@ -1,7 +1,7 @@
 class <%= class_name %> < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :http_authenticatable, :token_authenticatable, :lockable, :timeoutable and :activatable
-  devise :registerable, :authenticatable, :confirmable, :recoverable,
+  # :http_authenticatable, :token_authenticatable, :confirmable, :lockable, :timeoutable and :activatable
+  devise :registerable, :authenticatable, :recoverable,
          :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model

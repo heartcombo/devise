0609a5e19277d9a055bfa85464e8ab25c3592074
Mike Breen <hardbap@gmail.com>
2010-05-26 03:07:43 +0800

    :authenticatable as module is deprecated so the model generator should use :database_authenticatable

--- generators/devise/templates/model.rb
+++ generators/devise/templates/model.rb
@@ -1,7 +1,7 @@
 class <%= class_name %> < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :http_authenticatable, :token_authenticatable, :confirmable, :lockable, :timeoutable and :activatable
-  devise :registerable, :authenticatable, :recoverable,
+  devise :registerable, :database_authenticatable, :recoverable,
          :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model

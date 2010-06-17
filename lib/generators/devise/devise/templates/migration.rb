7dbd2eac2a7174cb7985b63c5c642f1e587d3e1d
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2010-06-17 01:57:36 -0300

    Confirmable is not default anymore, so remove it from generated migration.

--- lib/generators/devise/devise/templates/migration.rb
+++ lib/generators/devise/devise/templates/migration.rb
@@ -2,11 +2,11 @@ class DeviseCreate<%= table_name.camelize %> < ActiveRecord::Migration
   def self.up
     create_table(:<%= table_name %>) do |t|
       t.database_authenticatable :null => false
-      t.confirmable
       t.recoverable
       t.rememberable
       t.trackable
 
+      # t.confirmable
       # t.lockable :lock_strategy => :<%= Devise.lock_strategy %>, :unlock_strategy => :<%= Devise.unlock_strategy %>
       # t.token_authenticatable
 
@@ -14,8 +14,8 @@ class DeviseCreate<%= table_name.camelize %> < ActiveRecord::Migration
     end
 
     add_index :<%= table_name %>, :email,                :unique => true
-    add_index :<%= table_name %>, :confirmation_token,   :unique => true
     add_index :<%= table_name %>, :reset_password_token, :unique => true
+    # add_index :<%= table_name %>, :confirmation_token,   :unique => true
     # add_index :<%= table_name %>, :unlock_token,         :unique => true
   end
 

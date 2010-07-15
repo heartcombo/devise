5d6900dcdb4b1a98e7a17e9d3da842bfa7305f7a
José Valim <jose.valim@gmail.com>
2010-07-15 11:14:18 +0200

    Remove old generators.

--- lib/generators/devise_install_generator.rb
+++
@@ -1,4 +0,0 @@
-# Remove this file after deprecation
-if caller.none? { |l| l =~ %r{lib/rails/generators\.rb:(\d+):in `lookup!'$} }
-  warn "[WARNING] `rails g devise_install` is deprecated, please use `rails g devise:install` instead."
-end

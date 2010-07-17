3beb6ef1a681238b349615592af3c0c943d4ff10
José Valim <jose.valim@gmail.com>
2010-07-17 09:43:35 +0200

    Do not eager load all controllers. Otherwise this will force OAuth to be loaded.

--- lib/devise/rails.rb
+++ lib/devise/rails.rb
@@ -5,6 +5,12 @@ module Devise
   class Engine < ::Rails::Engine
     config.devise = Devise
 
+    # Skip eager load of controllers because it is handled by Devise
+    # to avoid loading unused controllers.
+    config.paths.app.controllers.autoload!
+    config.paths.app.controllers.skip_eager_load!
+
+    # Initialize Warden and copy its configurations.
     config.app_middleware.use Warden::Manager do |config|
       Devise.warden_config = config
     end
@@ -25,6 +31,20 @@ module Devise
       if Devise.oauth_providers.any?
         Devise.include_helpers(Devise::Oauth)
       end
+    end
+
+    # Check all available mapings and only load related controllers.
+    def eager_load!
+      mappings    = Devise.mappings.values.map(&:modules).flatten.uniq
+      controllers = Devise::CONTROLLERS.values_at(*mappings)
+      path        = paths.app.controllers.to_a.first
+      matcher     = /\A#{Regexp.escape(path)}\/(.*)\.rb\Z/
+
+      Dir.glob("#{path}/devise/{#{controllers.join(',')}}_controller.rb").sort.each do |file|
+        require_dependency file.sub(matcher, '\1')
+      end
+
+      super
     end
   end
 end

5aeb8cf1cf497e675dfd598b6e79f1ac4e7da139
Stefan Huber <MSNexploder@gmail.com>
2010-07-22 23:38:37 +0800

    small documentation fix

--- lib/generators/devise/templates/devise.rb
+++ lib/generators/devise/templates/devise.rb
@@ -102,7 +102,7 @@ Devise.setup do |config|
 
   # ==> Scopes configuration
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
-  # "sessions/users/new". It's turned off by default because it's slower if you
+  # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
   # config.scoped_views = true
 

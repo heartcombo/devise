f60123ce0102d6c724d693a6b2867a204c2def54
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2010-09-26 14:45:00 -0300

    Bump webrat to 0.7.1

--- test/support/webrat/integrations/rails.rb
+++ test/support/webrat/integrations/rails.rb
@@ -1,9 +1,9 @@
-require 'webrat/core/elements/field'
+require 'webrat/core/elements/form'
 require 'action_dispatch/testing/integration'
 
 module Webrat
-  Field.class_eval do
-    def parse_rails_request_params(params)
+  Form.class_eval do
+    def self.parse_rails_request_params(params)
       Rack::Utils.parse_nested_query(params)
     end
   end
@@ -13,20 +13,5 @@ module ActionDispatch #:nodoc:
   IntegrationTest.class_eval do
     include Webrat::Methods
     include Webrat::Matchers
- 
-    # The Rails version of within supports passing in a model and Webrat
-    # will apply a scope based on Rails' dom_id for that model.
-    #
-    # Example:
-    #   within User.last do
-    #     click_link "Delete"
-    #   end
-    def within(selector_or_object, &block)
-      if selector_or_object.is_a?(String)
-        super
-      else
-        super('#' + RecordIdentifier.dom_id(selector_or_object), &block)
-      end
-    end
   end
 end

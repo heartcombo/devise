a0557764a0720a1d29ba34200150365430e8611e
Pat Allan <pat@freelancing-gods.com>
2010-09-29 14:21:07 +1000

    Making sure timeoutable respects rememberable if both are loaded.

--- lib/devise/models/timeoutable.rb
+++ lib/devise/models/timeoutable.rb
@@ -22,9 +22,19 @@ module Devise
 
       # Checks whether the user session has expired based on configured time.
       def timedout?(last_access)
+        return false if remember_exists_and_not_expired?
+        
         last_access && last_access <= self.class.timeout_in.ago
       end
-
+      
+      private
+      
+      def remember_exists_and_not_expired?
+        return false unless respond_to?(:remember_expired?)
+        
+        remember_created_at && !remember_expired?
+      end
+      
       module ClassMethods
         Devise::Models.config(self, :timeout_in)
       end

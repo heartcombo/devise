bca72ddf59ee1f26a2d12427272990d84ee69a3a
George Guimarães <george@plataformatec.com.br>
2010-10-16 20:17:39 -0300

    fixing typo and whitespaces

--- lib/devise/encryptors/restful_authentication_sha1.rb
+++ lib/devise/encryptors/restful_authentication_sha1.rb
@@ -5,10 +5,10 @@ module Devise
     # = RestfulAuthenticationSha1
     # Simulates Restful Authentication's default encryption mechanism.
     # Warning: it uses Devise's pepper to port the concept of REST_AUTH_SITE_KEY
-    # Warning: it uses Devise's stretches configuration to port the concept of REST_AUTH_DIGEST_STRETCHES. Should be set to 10 in 
-    # the initializer to silumate the default behavior.
+    # Warning: it uses Devise's stretches configuration to port the concept of REST_AUTH_DIGEST_STRETCHES. Should be set to 10 in
+    # the initializer to simulate the default behavior.
     class RestfulAuthenticationSha1 < Base
-      
+
       # Gererates a default password digest based on salt, pepper and the
       # incoming password.
       def self.digest(password, stretches, salt, pepper)

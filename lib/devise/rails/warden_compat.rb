0f46ed3a338010dbd4ead16671e5ad1868c421ef
José Valim <jose.valim@gmail.com>
2010-11-25 00:04:36 +0100

    Fix indifferent access on session issue.

--- lib/devise/rails/warden_compat.rb
+++ lib/devise/rails/warden_compat.rb
@@ -39,6 +39,63 @@ class Warden::SessionSerializer
 end
 
 unless Devise.rack_session?
+  # We cannot use Rails Indifferent Hash because it messes up the flash object.
+  class Devise::IndifferentHash < Hash
+    alias_method :regular_writer, :[]= unless method_defined?(:regular_writer)
+    alias_method :regular_update, :update unless method_defined?(:regular_update)
+
+    def []=(key, value)
+      regular_writer(convert_key(key), value)
+    end
+
+    alias_method :store, :[]=
+
+    def update(other_hash)
+      other_hash.each_pair { |key, value| regular_writer(convert_key(key), value) }
+      self
+    end
+
+    alias_method :merge!, :update
+
+    def key?(key)
+      super(convert_key(key))
+    end
+
+    alias_method :include?, :key?
+    alias_method :has_key?, :key?
+    alias_method :member?, :key?
+
+    def fetch(key, *extras)
+      super(convert_key(key), *extras)
+    end
+
+    def values_at(*indices)
+      indices.collect {|key| self[convert_key(key)]}
+    end
+
+    def merge(hash)
+      self.dup.update(hash)
+    end
+
+    def delete(key)
+      super(convert_key(key))
+    end
+
+    def stringify_keys!; self end
+    def stringify_keys; dup end
+
+    undef :symbolize_keys!
+    def symbolize_keys; to_hash.symbolize_keys end
+
+    def to_options!; self end
+
+    protected
+
+    def convert_key(key)
+      key.kind_of?(Symbol) ? key.to_s : key
+    end
+  end
+
   class ActionDispatch::Request
     def reset_session
       session.destroy if session && session.respond_to?(:destroy)
@@ -54,7 +111,7 @@ unless Devise.rack_session?
       backup.delete("session_id")
       request.reset_session
       warden.env['action_dispatch.request.flash_hash'] = flash
-      request.session.update(backup)
+      request.session = Devise::IndifferentHash.new.update(backup)
     end
   end
 end

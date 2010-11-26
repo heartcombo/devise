112f49993701d75e946d5232bf69d970c3e62b74
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2010-11-26 00:02:50 -0200

    Use UTC for Mongoid timestamps, so it conforms with AR.

--- test/orm/mongoid.rb
+++ test/orm/mongoid.rb
@@ -1,5 +1,6 @@
 Mongoid.configure do |config|
-  config.master = Mongo::Connection.new('127.0.0.1', 27017).db("devise-test-suite")
+  config.master  = Mongo::Connection.new('127.0.0.1', 27017).db("devise-test-suite")
+  config.use_utc = true
 end
 
 class ActiveSupport::TestCase

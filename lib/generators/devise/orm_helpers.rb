2c1106a430b24baeb9c0a74f1b14baa893148d4c
Andrés Mejía <andmej@gmail.com>
2010-12-09 23:03:48 +0100

    Removing caching from #model_exists? so it doesn't return a false positive when the method is called, the file is deleted and then the method is called again.

--- lib/generators/devise/orm_helpers.rb
+++ lib/generators/devise/orm_helpers.rb
@@ -12,8 +12,7 @@ CONTENT
       end
 
       def model_exists?
-        return @model_exists if instance_variable_defined?(:@model_exists)
-        @model_exists = File.exists?(File.join(destination_root, model_path))
+        File.exists?(File.join(destination_root, model_path))
       end
 
       def model_path

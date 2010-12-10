a2e92a2ef2ae6b8178ad705f03fa2f098e99a33b
Andrés Mejía <andmej@gmail.com>
2010-12-10 22:51:28 +0800

    Adding mongoid generator tests.

--- test/generators/mongoid_generator_test.rb
+++ test/generators/mongoid_generator_test.rb
@@ -1,22 +1,20 @@
 require File.expand_path("../generators_test_helper", __FILE__)
 
-if DEVISE_ORM == :active_record
-  class ActiveRecordGeneratorTest < Rails::Generators::TestCase
-    tests ActiveRecord::Generators::DeviseGenerator
+if DEVISE_ORM == :mongoid
+  class MongoidGeneratorTest < Rails::Generators::TestCase
+    tests Mongoid::Generators::DeviseGenerator
     destination File.expand_path("../../tmp", __FILE__)
     setup :prepare_destination
   
     test "all files are properly created" do
       run_generator %w(monster)
-      assert_file "app/models/monster.rb", /devise/, /attr_accessible (:[a-z_]+(, )?)+/
-      assert_migration "db/migrate/devise_create_monsters.rb"
+      assert_file "app/models/monster.rb", /devise/
     end
   
     test "all files are properly deleted" do
       run_generator %w(monster)
       run_generator %w(monster), :behavior => :revoke
       assert_no_file "app/models/monster.rb"
-      assert_no_migration "db/migrate/devise_create_monsters.rb"
     end
   end
 end

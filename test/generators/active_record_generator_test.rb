60be68436bb0ae14980d42c49453f612e6ffb96a
Andrés Mejía <andmej@gmail.com>
2010-12-10 22:17:51 +0800

    Adding tests for Active Record generator

---
+++ test/generators/active_record_generator_test.rb
@@ -0,0 +1,20 @@
+require File.expand_path("../generators_test_helper", __FILE__)
+
+class ActiveRecordGeneratorTest < Rails::Generators::TestCase
+  tests ActiveRecord::Generators::DeviseGenerator
+  destination File.expand_path("../../tmp", __FILE__)
+  setup :prepare_destination
+  
+  test "all files are properly created" do
+    run_generator %w(monster)
+    assert_file "app/models/monster.rb", /devise/, /attr_accessible (:[a-z_]+(, )?)+/
+    assert_migration "db/migrate/devise_create_monsters.rb"
+  end
+  
+  test "all files are properly deleted" do
+    run_generator %w(monster)
+    run_generator %w(monster), :behavior => :revoke
+    assert_no_file "app/models/monster.rb"
+    assert_no_migration "db/migrate/devise_create_monsters.rb"
+  end
+end

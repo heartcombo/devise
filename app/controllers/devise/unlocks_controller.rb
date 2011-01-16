336ccc7cf2cd300890fa0bf18aa2ff204c094b2c
Prem Sichanugrist <s@sikachu.com>
2011-01-16 19:20:19 +0700

    Add support for non-navigational formats in UnlocksController

--- app/controllers/devise/unlocks_controller.rb
+++ app/controllers/devise/unlocks_controller.rb
@@ -13,10 +13,12 @@ class Devise::UnlocksController < ApplicationController
     self.resource = resource_class.send_unlock_instructions(params[resource_name])
 
     if resource.errors.empty?
-      set_flash_message :notice, :send_instructions
-      redirect_to new_session_path(resource_name)
+      set_flash_message :notice, :send_instructions if is_navigational_format?
+      respond_with resource, :location => new_session_path(resource_name)
     else
-      render_with_scope :new
+      respond_with(resource) do |format|
+        format.any(*navigational_formats) { render_with_scope :new }
+      end
     end
   end
 
@@ -25,10 +27,15 @@ class Devise::UnlocksController < ApplicationController
     self.resource = resource_class.unlock_access_by_token(params[:unlock_token])
 
     if resource.errors.empty?
-      set_flash_message :notice, :unlocked
-      sign_in_and_redirect(resource_name, resource)
+      set_flash_message :notice, :unlocked if is_navigational_format?
+      sign_in(resource_name, resource)
+      respond_with(resource) do |format|
+        format.any(*navigational_formats) { redirect_to redirect_location(resource_name, resource) }
+      end
     else
-      render_with_scope :new
+      respond_with(resource.errors, :status => :unprocessable_entity) do |format|
+        format.any(*navigational_formats) { render_with_scope :new }
+      end
     end
   end
 end

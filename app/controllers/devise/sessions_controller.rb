32c9cce79de0edd4cf1acc60572f824935cd6731
Prem Sichanugrist <s@sikachu.com>
2011-01-16 20:31:37 +0700

    Add support for `destory_user_session_path` in another non-navigational formats such as JSON and XML

--- app/controllers/devise/sessions_controller.rb
+++ app/controllers/devise/sessions_controller.rb
@@ -19,7 +19,15 @@ class Devise::SessionsController < ApplicationController
   # GET /resource/sign_out
   def destroy
     signed_in = signed_in?(resource_name)
-    sign_out_and_redirect(resource_name)
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
     set_flash_message :notice, :signed_out if signed_in
+
+    # We actually need to hard coded this, as Rails default responder doesn't
+    # support returning empty response on GET request
+    respond_to do |format|
+      format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name) }
+      format.xml { head :ok }
+      format.json { render :text => '{}', :status => :ok }
+    end
   end
 end

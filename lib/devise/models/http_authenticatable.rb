de22a30834db7a907de5a4d7da047715b4d03b3b
Eric Cohen <eric@rento.gr>
2011-01-21 22:36:31 +0800

    Added configuration of authentication_keys to http authenticatable model module.
    Closes #799.

--- lib/devise/models/http_authenticatable.rb
+++ lib/devise/models/http_authenticatable.rb
@@ -3,14 +3,16 @@ require 'devise/strategies/http_authenticatable'
 module Devise
   module Models
     # Adds HttpAuthenticatable behavior to your model. It expects that your
-    # model class responds to authenticate and authentication_keys methods
-    # (which for example are defined in authenticatable).
+    # model class responds to authenticate method
+    # (which for example is defined in authenticatable).
     module HttpAuthenticatable
       def self.included(base)
         base.extend ClassMethods
       end
 
       module ClassMethods
+        Devise::Models.config(self, :authentication_keys)
+
         # Authenticate an user using http.
         def authenticate_with_http(username, password)
           authenticate(authentication_keys.first => username, :password => password)

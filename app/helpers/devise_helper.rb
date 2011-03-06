63b72cd512b14ebe5bbf48eb80d1aba39978735e
José Valim <jose.valim@gmail.com>
2011-03-06 05:25:09 -0800

    Edited app/helpers/devise_helper.rb via GitHub

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,4 +1,10 @@
 module DeviseHelper
+  # A simple way to show error messages for the current devise resource. If you need
+  # to customize this method, you can either overwrite it in your application helpers or
+  # copy the views to your application.
+  #
+  # This method is intended to stay simple and it is unlikely that we are going to change
+  # it to add more behavior or options.
   def devise_error_messages!
     return "" if resource.errors.empty?
 

30dcac54829904c23fd1e5bb32699f2c8c3c6160
Roland Lu <rolandlu7@gmail.com>
2011-03-06 08:40:20 +0800

    Add I8n to devise_error_messages! message keys

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -2,7 +2,7 @@ module DeviseHelper
   def devise_error_messages!
     return "" if resource.errors.empty?
 
-    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    messages = resource.errors.map { |key, msg| content_tag(:li, I18n.t(key) + msg) }.join
     sentence = I18n.t("errors.messages.not_saved",
                       :count => resource.errors.count,
                       :resource => resource_name)

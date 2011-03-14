d58a72ee32d74e354d797c330a4334388afd4643
José Valim <jose.valim@gmail.com>
2011-03-14 11:22:09 +0100

    Remove stuff deprecated on .rc2

--- lib/devise/omniauth/config.rb
+++ lib/devise/omniauth/config.rb
@@ -13,18 +13,6 @@ module Devise
       def strategy_class
         ::OmniAuth::Strategies.const_get("#{::OmniAuth::Utils.camelize(@provider.to_s)}")
       end
-
-      def check_if_allow_stubs!
-        raise "OmniAuth strategy for #{@provider} does not allow stubs, only OAuth2 ones do." unless allow_stubs?
-      end
-
-      def allow_stubs?
-        defined?(::OmniAuth::Strategies::OAuth2) && strategy.is_a?(::OmniAuth::Strategies::OAuth2)
-      end
-
-      def build_connection(&block)
-        strategy.client.connection.build(&block)
-      end
     end
   end
 end     

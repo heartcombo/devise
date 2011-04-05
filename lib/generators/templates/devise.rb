4461743ae39a8542b6c2f38e89986f777b3b2336
Gavin Miller <gavingmiller@gmail.com>
2011-04-04 22:47:56 -0600

    Added conditional to reduce test cost.

--- lib/generators/templates/devise.rb
+++ lib/generators/templates/devise.rb
@@ -51,7 +51,7 @@ Devise.setup do |config|
   # ==> Configuration for :database_authenticatable
   # For bcrypt, this is the cost for hashing the password and defaults to 10. If
   # using other encryptors, it sets how many times you want the password re-encrypted.
-  config.stretches = 10
+  config.stretches = (Rails.env == "test") ? 1 : 10
 
   # Setup a pepper to generate the encrypted password.
   # config.pepper = <%= ActiveSupport::SecureRandom.hex(64).inspect %>

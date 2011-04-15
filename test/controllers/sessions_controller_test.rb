e4af6adaf7e39aeeb02b16232911966d6599dc56
John Wilger <johnwilger@gmail.com>
2011-04-15 08:57:56 +0800

    Added test showing previously incorrect behavior of Devise::SessionsController#create

---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,17 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'test_helper'))
+
+class SessionsControllerTest < ActionController::TestCase
+  tests Devise::SessionsController
+  include Devise::TestHelpers
+
+  test "#create doesn't raise exception after Warden authentication fails " \
+     + "when TestHelpers included" do
+    request.env["devise.mapping"] = Devise.mappings[:user]
+    assert_nothing_raised(NoMethodError) do
+      post :create, :user => {
+        :email => "nosuchuser@example.com",
+        :password => "wevdude"
+      }
+    end
+  end
+end

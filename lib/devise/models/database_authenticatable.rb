e329930a821f81f77d3e581f0f9aef4d5c9a3cf3
Mikel Lindsaar <mikel@rubyx.com>
2011-04-16 16:13:17 +1000

    Update DatabaseAuthenticatable#valid_password? to not raise error on empty password
    Signed-off-by: José Valim <jose.valim@gmail.com>

--- lib/devise/models/database_authenticatable.rb
+++ lib/devise/models/database_authenticatable.rb
@@ -33,9 +33,13 @@ module Devise
 
       # Verifies whether an password (ie from sign in) is the user password.
       def valid_password?(password)
-        bcrypt   = ::BCrypt::Password.new(self.encrypted_password)
-        password = ::BCrypt::Engine.hash_secret("#{password}#{self.class.pepper}", bcrypt.salt)
-        Devise.secure_compare(password, self.encrypted_password)
+        begin
+          bcrypt   = ::BCrypt::Password.new(self.encrypted_password)
+          password = ::BCrypt::Engine.hash_secret("#{password}#{self.class.pepper}", bcrypt.salt)
+          Devise.secure_compare(password, self.encrypted_password)
+        rescue BCrypt::Errors::InvalidHash
+          return false
+        end
       end
 
       # Set password and password confirmation to nil

34a54dac2d086431bbbb667f698f18c33f0b765b
José Valim <jose.valim@gmail.com>
2011-04-16 12:43:43 +0200

    Simply check instead or rescueing.

--- lib/devise/models/database_authenticatable.rb
+++ lib/devise/models/database_authenticatable.rb
@@ -33,13 +33,10 @@ module Devise
 
       # Verifies whether an password (ie from sign in) is the user password.
       def valid_password?(password)
-        begin
-          bcrypt   = ::BCrypt::Password.new(self.encrypted_password)
-          password = ::BCrypt::Engine.hash_secret("#{password}#{self.class.pepper}", bcrypt.salt)
-          Devise.secure_compare(password, self.encrypted_password)
-        rescue BCrypt::Errors::InvalidHash
-          return false
-        end
+        return false if encrypted_password.blank?
+        bcrypt   = ::BCrypt::Password.new(self.encrypted_password)
+        password = ::BCrypt::Engine.hash_secret("#{password}#{self.class.pepper}", bcrypt.salt)
+        Devise.secure_compare(password, self.encrypted_password)
       end
 
       # Set password and password confirmation to nil

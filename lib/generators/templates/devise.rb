9fe0cb4954b9fa8d9bdbbff4452cabe8a602db02
José Valim <jose.valim@gmail.com>
2011-04-18 09:59:54 +0200

    use @example.com

--- lib/generators/templates/devise.rb
+++ lib/generators/templates/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = "please-change-me@config-initializers-devise.com"
+  config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"

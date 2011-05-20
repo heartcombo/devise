004fe2ad125b4d8c311fb9a12cda6ab6d49155d3
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2011-05-20 19:41:40 -0300

    Paranoid mode for recoverable

--- app/controllers/devise/passwords_controller.rb
+++ app/controllers/devise/passwords_controller.rb
@@ -11,14 +11,19 @@ class Devise::PasswordsController < ApplicationController
   # POST /resource/password
   def create
     self.resource = resource_class.send_reset_password_instructions(params[resource_name])
-
-    if resource.errors.empty?
+    if Devise.paranoid
       set_flash_message(:notice, :send_instructions) if is_navigational_format?
       respond_with resource, :location => after_sending_reset_password_instructions_path_for(resource_name)
     else
-      respond_with_navigational(resource){ render_with_scope :new }
+      if resource.errors.empty?
+        set_flash_message(:notice, :send_instructions) if is_navigational_format?
+        respond_with resource, :location => new_session_path(resource_name)
+      else
+        respond_with_navigational(resource){ render_with_scope :new }
+      end
     end
   end
+
 
   # GET /resource/password/edit?reset_password_token=abcdef
   def edit
--- lib/devise/controllers/internal_helpers.rb
+++ lib/devise/controllers/internal_helpers.rb
@@ -115,6 +115,9 @@ MESSAGE
       # available.
       def set_flash_message(key, kind, options={}) #:nodoc:
         options[:scope] = "devise.#{controller_name}"
+        if Devise.paranoid && controller_name == "passwords"
+          options[:scope] += ".paranoid"
+        end
         options[:default] = Array(options[:default]).unshift(kind.to_sym)
         options[:resource_name] = resource_name
         message = I18n.t("#{resource_name}.#{kind}", options)

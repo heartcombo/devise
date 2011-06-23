2ae2fdc9f838ccf65b0154d9f304288064bfc685
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2011-06-22 21:27:20 -0300

    Returning an empty JSON for creating confirmation requests

--- app/controllers/devise/confirmations_controller.rb
+++ app/controllers/devise/confirmations_controller.rb
@@ -13,7 +13,7 @@ class Devise::ConfirmationsController < ApplicationController
 
     if successful_and_sane?(resource)
       set_flash_message(:notice, :send_instructions) if is_navigational_format?
-      respond_with resource, :location => after_resending_confirmation_instructions_path_for(resource_name)
+      respond_with({}, :location => after_resending_confirmation_instructions_path_for(resource_name))
     else
       respond_with_navigational(resource){ render_with_scope :new }
     end

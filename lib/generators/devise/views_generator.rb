29a0af78bcfbea14734ab6387c7155e045422aef
José Valim <jose.valim@gmail.com>
2011-06-27 11:20:32 -0300

    Refactor view generators.

--- lib/generators/devise/views_generator.rb
+++ lib/generators/devise/views_generator.rb
@@ -1,19 +1,67 @@
-require 'tmpdir'
-
 module Devise
   module Generators
     class ViewsGenerator < Rails::Generators::Base
-      source_root File.expand_path("../../../../app/views", __FILE__)
-      desc "Copies all Devise views to your application."
+      desc "Copies Devise views to your application."
 
       argument :scope, :required => false, :default => nil,
                        :desc => "The scope to copy views to"
 
-      # class_option :template_engine, :type => :string, :aliases => "-t",
-      #                                :desc => "Template engine for the views. Available options are 'erb', 'haml' and 'slim'."
+      class_option :form_builder, :type => :string, :aliases => "-b",
+                                  :desc => "Form builder to be used",
+                                  :default => defined?(SimpleForm) ? "simple_form_for" : "form_for"
 
       def copy_views
-        directory "devise", "app/views/#{scope || :devise}"
+        invoke SharedViewsGenerator
+
+        if options[:form_builder] == "form_for"
+          invoke DefaultViewsGenerator
+        else
+          invoke SimpleFormViewsGenerator
+        end
+      end
+    end
+
+    module ViewPathTemplates
+      extend ActiveSupport::Concern
+
+      included do
+        source_root File.expand_path("../../../../app/views", __FILE__)
+
+        argument :scope, :required => false, :default => nil,
+                         :desc => "The scope to copy views to"
+      end
+
+      protected
+
+      def view_directory(name)
+        directory "devise/#{name}", "#{target_path}/#{name}"
+      end
+
+      def target_path
+        @target_path ||= "app/views/#{scope || :devise}"
+      end
+    end
+
+    class SharedViewsGenerator < Rails::Generators::Base
+      include ViewPathTemplates
+      desc "Copies shared Devise views to your application."
+
+      def copy_views
+        view_directory :mailer
+        view_directory :shared
+      end
+    end
+
+    class DefaultViewsGenerator < Rails::Generators::Base
+      include ViewPathTemplates
+      desc "Copies default Devise views to your application."
+
+      def copy_views
+        view_directory :confirmations
+        view_directory :passwords
+        view_directory :registrations
+        view_directory :sessions
+        view_directory :unlocks
       end
     end
   end

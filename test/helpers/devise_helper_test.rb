219fbc00561099b1b6a2b621b4e397cf764946f8
José Valim <jose.valim@gmail.com>
2011-06-28 07:05:35 -0300

    Fix tests, remove whitespace and update CHANGELOG.

--- test/helpers/devise_helper_test.rb
+++ test/helpers/devise_helper_test.rb
@@ -10,14 +10,17 @@ class DeviseHelperTest < ActionController::IntegrationTest
       } } },
       :activerecord => { :models => { :user => "utilisateur" } }
     }
-  
+
     I18n.locale = 'fr'
   end
-  
+
+  teardown do
+    I18n.locale = 'en'
+  end
+
   test 'test errors.messages.not_saved with single error from i18n' do
-    
     get new_user_registration_path
-    
+
     fill_in 'password', :with => 'new_user123'
     fill_in 'password confirmation', :with => 'new_user123'
     click_button 'Sign up'
@@ -25,10 +28,10 @@ class DeviseHelperTest < ActionController::IntegrationTest
     assert_have_selector '#error_explanation'
     assert_contain "Erreur lors de l'enregistrement de 'utilisateur': 1 erreur"
   end
-  
+
   test 'test errors.messages.not_saved with multiple errors from i18n' do
     get new_user_registration_path
-    
+
     fill_in 'email', :with => 'invalid_email'
     fill_in 'password', :with => 'new_user123'
     fill_in 'password confirmation', :with => 'new_user321'
@@ -37,5 +40,4 @@ class DeviseHelperTest < ActionController::IntegrationTest
     assert_have_selector '#error_explanation'
     assert_contain "Erreur lors de l'enregistrement de 'utilisateur': 2 erreurs"
   end
-  
 end

2a6c160d3957fe7960fa8e04dea682246a3a7a2c
Jo Liss <joliss42@gmail.com>
2011-07-08 18:41:16 +0200

    Remove whitespace from generator template

--- test/rails_app/config/initializers/devise.rb
+++ test/rails_app/config/initializers/devise.rb
@@ -35,7 +35,7 @@ Devise.setup do |config|
   # These keys will be downcased upon creating or modifying a user and when used
   # to authenticate or find a user. Default is :email.
   config.case_insensitive_keys = [ :email ]
-  
+
   # Configure which authentication keys should have whitespace stripped.
   # These keys will have whitespace before and after removed upon creating or
   # modifying a user and when used to authenticate or find a user. Default is :email.

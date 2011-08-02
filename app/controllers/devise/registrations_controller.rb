7264bb55b2ca77c3765990ffb0c402e04b7d5f62
hck <hck@hck-hp.(none)>
2011-08-02 08:11:25 +0300

    added I18n support to inactive_message in RegistrationsController

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -19,7 +19,7 @@ class Devise::RegistrationsController < ApplicationController
         sign_in(resource_name, resource)
         respond_with resource, :location => redirect_location(resource_name, resource)
       else
-        set_flash_message :notice, :inactive_signed_up, :reason => resource.inactive_message.to_s if is_navigational_format?
+        set_flash_message :notice, :inactive_signed_up, :reason => I18n.t("devise.registrations.reasons." << resource.inactive_message.to_s) if is_navigational_format?
         expire_session_data_after_sign_in!
         respond_with resource, :location => after_inactive_sign_up_path_for(resource)
       end

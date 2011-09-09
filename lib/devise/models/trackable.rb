df3e711ee01210fc144ed2ef949a88acd10a3ddd
Oleg Shparber <trollixx@gmail.com>
2011-09-09 12:46:35 +0300

    Typo

--- lib/devise/models/trackable.rb
+++ lib/devise/models/trackable.rb
@@ -5,7 +5,7 @@ module Devise
     # Track information about your user sign in. It tracks the following columns:
     #
     # * sign_in_count      - Increased every time a sign in is made (by form, openid, oauth)
-    # * current_sign_in_at - A tiemstamp updated when the user signs in
+    # * current_sign_in_at - A timestamp updated when the user signs in
     # * last_sign_in_at    - Holds the timestamp of the previous sign in
     # * current_sign_in_ip - The remote ip updated when the user sign in
     # * last_sign_in_ip    - Holds the remote ip of the previous sign in

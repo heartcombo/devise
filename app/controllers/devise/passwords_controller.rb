b98720d324f8388e5718a671250057ce3e140d3e
Jim Herzberg <jimmiesh@gmail.com>
2011-10-12 14:12:20 -0700

    jh - reworking paranoid mode in passwords controller
    Signed-off-by: José Valim <jose.valim@gmail.com>

--- app/controllers/devise/passwords_controller.rb
+++ app/controllers/devise/passwords_controller.rb
@@ -12,8 +12,7 @@ class Devise::PasswordsController < ApplicationController
   def create
     self.resource = resource_class.send_reset_password_instructions(params[resource_name])
 
-    if successful_and_sane?(resource)
-      set_flash_message(:notice, :send_instructions) if is_navigational_format?
+    if successfully_sent?(resource)
       respond_with({}, :location => after_sending_reset_password_instructions_path_for(resource_name))
     else
       respond_with_navigational(resource){ render_with_scope :new }

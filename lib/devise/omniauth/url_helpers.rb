3834f36d400f82bd660444e2b115de15650e88af
José Valim <jose.valim@gmail.com>
2011-10-18 08:35:19 +0200

    Another attempt to fix the misterious loading helpers bug.

--- lib/devise/omniauth/url_helpers.rb
+++ lib/devise/omniauth/url_helpers.rb
@@ -3,10 +3,9 @@ module Devise
     module UrlHelpers
       def self.define_helpers(mapping)
         return unless mapping.omniauthable?
-        method = "#{mapping.name}_omniauth_authorize_path"
 
         class_eval <<-URL_HELPERS, __FILE__, __LINE__ + 1
-          def #{method}(provider, params = {})
+          def #{mapping.name}_omniauth_authorize_path(provider, params = {})
             if Devise.omniauth_configs[provider.to_sym]
               script_name = request.env["SCRIPT_NAME"]
 
@@ -17,11 +16,8 @@ module Devise
               raise ArgumentError, "Could not find omniauth provider \#{provider.inspect}"
             end
           end
-          protected :#{method}
         URL_HELPERS
       end
-
-      protected
 
       def omniauth_authorize_path(resource_or_scope, *args)
         scope = Devise::Mapping.find_scope!(resource_or_scope)

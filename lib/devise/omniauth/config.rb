6b5958a7722542f46f371bc87794724c3619b4c1
TamiasSibiricus <denys.kyselov@gmail.com>
2011-11-07 20:59:07 +0200

    Update dependencies for OmniAuth 1.0.0 stable

--- lib/devise/omniauth/config.rb
+++ lib/devise/omniauth/config.rb
@@ -2,22 +2,44 @@ module Devise
   module OmniAuth
     class Config
       attr_accessor :strategy
-      attr_reader :args
+      attr_reader :args, :options, :provider
 
       def initialize(provider, args)
         @provider = provider
         @args     = args
         @strategy = nil
+        @options = @args.last.is_a?(Hash) ? @args.last : {}
       end
 
       # open_id strategy can have configurable name
       def strategy_name
-        options = @args.last.is_a?(Hash) && @args.last
-        options && options[:name] ? options[:name] : @provider
+        options[:name] || @provider
       end
 
       def strategy_class
-        ::OmniAuth::Strategies.const_get("#{::OmniAuth::Utils.camelize(@provider.to_s)}")
+        find_strategy || require_strategy
+      end
+
+      def find_strategy
+        ::OmniAuth.strategies.find do |strategy_class|
+          strategy_class.to_s =~ /#{::OmniAuth::Utils.camelize(strategy_name)}$/ ||
+            strategy_class.default_options[:name] == strategy_name
+        end
+      end
+
+      def require_strategy
+        if [:facebook, :github, :twitter].include?(provider.to_sym)
+          require "omniauth/strategies/#{provider}"
+        elsif options[:require]
+          require options[:require]
+        else
+          require "omniauth-#{provider}"
+        end
+        find_strategy || autoload_strategy
+      end
+
+      def autoload_strategy
+        ::OmniAuth::Strategies.const_get(::OmniAuth::Utils.camelize(provider.to_s))
       end
     end
   end

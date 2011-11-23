9c3d5705b5bbb0eb2e9a98e856cbaebc409f3d01
José Valim <jose.valim@gmail.com>
2011-11-23 08:42:45 +0000

    Update lib/generators/templates/devise.rb

--- lib/generators/templates/devise.rb
+++ lib/generators/templates/devise.rb
@@ -1,5 +1,5 @@
-# Use this hook to configure devise mailer, warden hooks and so forth. The first
-# four configuration values can also be set straight in your models.
+# Use this hook to configure devise mailer, warden hooks and so forth.
+# Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,

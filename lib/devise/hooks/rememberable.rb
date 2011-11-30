d1e1e9df6280ce71613eb092d43f676e4e34cef7
Ian Warshak <iwarshak@stripey.net>
2011-11-30 14:12:11 -0600

    previously, if you are storing a model in a scope other than the default the remember_me functionality would break because the remember_X_token would not be named properly
    for instance, if you are authenticating a User, but using the scope 'mobile_user',
    the remember token would (incorrectly) be set to remember_user_token, meaning
    that the cookie would not be used and breaking the remember me functionality

--- lib/devise/hooks/rememberable.rb
+++ lib/devise/hooks/rememberable.rb
@@ -1,6 +1,6 @@
 Warden::Manager.after_set_user :except => :fetch do |record, warden, options|
   scope = options[:scope]
   if record.respond_to?(:remember_me) && record.remember_me && warden.authenticated?(scope)
-    Devise::Controllers::Rememberable::Proxy.new(warden).remember_me(record)
+    Devise::Controllers::Rememberable::Proxy.new(warden).remember_me(record, :scope => scope)
   end
 end

f41e4befde40ff0be7cc9560e7b5c12629350ae8
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2011-12-07 12:01:16 +0200

    Fix taking associated failure app from the scope in the given env.
    There is a delegator to get failure app, introduced in 4629bee and tuned
    in 24b26026. The latter commit introduced a bit of logic, however, no
    tests are included into commit. Needless to say this resulted in a
    broken code.
    
    The point is that `env["warden.options"][:scope]` returns a string.
    However, `Devise.mappings` is a hash with symbol keys.
    
    Adding tests and converting scope to symbol here.

--- lib/devise/delegator.rb
+++ lib/devise/delegator.rb
@@ -8,7 +8,7 @@ module Devise
     def failure_app(env)
       app = env["warden.options"] &&
         (scope = env["warden.options"][:scope]) &&
-        Devise.mappings[scope].failure_app
+        Devise.mappings[scope.to_sym].failure_app
 
       app || Devise::FailureApp
     end

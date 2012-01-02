8b3e0e52f179e0cbfbdee17fe69876201f79a9a3
José Valim <jose.valim@gmail.com>
2012-01-02 19:27:05 +0100

    Generate routes again for token, closes #1526.

--- lib/generators/templates/devise.rb
+++ lib/generators/templates/devise.rb
@@ -70,6 +70,9 @@ Devise.setup do |config|
 
   # By default Devise will store the user in session. You can skip storage for
   # :http_auth and :token_auth by adding those symbols to the array below.
+  # Notice that if you are skipping storage for all authentication paths, you
+  # may want to disable generating routes to Devise's sessions controller by
+  # passing :skip => :sessions to `devise_for` in your config/routes.rb
   config.skip_session_storage = [:http_auth]
 
   # ==> Configuration for :database_authenticatable

0cd18981f0165ab5db8020774d0fe2ca0e060a17
Damien Wilson <damien@change.org>
2012-03-19 13:21:27 -0700

    Add SCrypt encryptor.

---
+++ lib/devise/encryptors/scrypt.rb
@@ -0,0 +1,14 @@
+module Devise
+  module Encryptors
+    class SCrypt < Base
+      def self.digest(password, stretches, salt, pepper)
+        ::SCrypt::Engine.hash_secret("#{password}#{pepper}", salt)
+      end
+
+      def self.compare(encrypted_password, password, stretches, salt, pepper)
+        salt = ::SCrypt::Password.new(encrypted_password).salt
+        Devise.secure_compare(encrypted_password, digest(password, stretches, salt, pepper))
+      end
+    end
+  end
+end

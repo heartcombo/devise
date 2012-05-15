b6e30427a39c93fc47b592cdebe6b4f4cb62818a
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2012-05-15 17:19:21 -0300

    Pass the block argument straight to the assertion methods in test helpers

--- test/support/assertions.rb
+++ test/support/assertions.rb
@@ -15,14 +15,14 @@ class ActiveSupport::TestCase
   alias :assert_present :assert_not_blank
 
   def assert_email_sent(address = nil, &block)
-    assert_difference('ActionMailer::Base.deliveries.size') { yield }
+    assert_difference('ActionMailer::Base.deliveries.size', &block)
     if address.present?
       assert_equal address, ActionMailer::Base.deliveries.last['to'].to_s
     end
   end
 
   def assert_email_not_sent(&block)
-    assert_no_difference('ActionMailer::Base.deliveries.size') { yield }
+    assert_no_difference('ActionMailer::Base.deliveries.size', &block)
   end
 
   def assert_same_content(result, expected)
@@ -32,11 +32,9 @@ class ActiveSupport::TestCase
     end
   end
 
-  def assert_raise_with_message(exception_klass, message)
-    exception = assert_raise exception_klass do
-      yield
-    end
-
-    assert_equal exception.message, message, "The expected message was #{message} but your exception throwed #{exception.message}"
+  def assert_raise_with_message(exception_klass, message, &block)
+    exception = assert_raise exception_klass, &block
+    assert_equal exception.message, message,
+      "The expected message was #{message} but your exception throwed #{exception.message}"
   end
 end

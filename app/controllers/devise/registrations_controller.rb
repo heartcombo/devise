68dc20cba27369c35e3203b5c23485f2a919f85d
Philip Poots <philip.poots@gmail.com>
2012-10-23 20:05:45 +0100

    Separates sign_up and sign_in on Registration
    See #2104
    
    Completed with the help of @rubynortheast

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -15,7 +15,7 @@ class Devise::RegistrationsController < DeviseController
     if resource.save
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
-        sign_in(resource_name, resource)
+        sign_up(resource_name, resource)
         respond_with resource, :location => after_sign_up_path_for(resource)
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?

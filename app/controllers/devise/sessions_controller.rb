8fa3951beac6287de97133a4cdf34f3a2816afa8
José Valim <jose.valim@plataformatec.com.br>
2013-01-06 11:07:51 +0100

    Update app/controllers/devise/sessions_controller.rb

--- app/controllers/devise/sessions_controller.rb
+++ app/controllers/devise/sessions_controller.rb
@@ -27,9 +27,7 @@ class Devise::SessionsController < DeviseController
     # We actually need to hardcode this as Rails default responder doesn't
     # support returning empty response on GET request
     respond_to do |format|
-      format.all do
-        head :no_content
-      end
+      format.all { head :no_content }
       format.any(*navigational_formats) { redirect_to redirect_path }
     end
   end

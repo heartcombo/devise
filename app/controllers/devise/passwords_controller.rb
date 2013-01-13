580446abed06587e9659d71a535e480224420ae7
Danial Pearce <git@tigris.id.au>
2013-01-13 20:47:18 +1100

    Allow controller to pass params to reset password email

--- app/controllers/devise/passwords_controller.rb
+++ app/controllers/devise/passwords_controller.rb
@@ -10,7 +10,7 @@ class Devise::PasswordsController < DeviseController
 
   # POST /resource/password
   def create
-    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    self.resource = resource_class.send_reset_password_instructions(resource_params, mailer_params)
 
     if successfully_sent?(resource)
       respond_with({}, :location => after_sending_reset_password_instructions_path_for(resource_name))

d9513e3beae833e1958d3b7207102df107a96139
Rafael Mendonça França <rafaelmfranca@gmail.com>
2013-01-28 13:21:44 -0200

    Use the Ruby 1.8 hash syntax.
    Yes we still support Ruby 1.8 :cry:
    
    Conflicts:
    	Gemfile.lock

--- test/models/authenticatable_test.rb
+++ test/models/authenticatable_test.rb
@@ -6,8 +6,8 @@ class AuthenticatableTest < ActiveSupport::TestCase
   end
 
   test 'find_first_by_auth_conditions allows custom filtering parameters' do
-    user = User.create!(email: "example@example.com", password: "123456")
-    assert_equal User.find_first_by_auth_conditions({ email: "example@example.com" }), user
-    assert_equal User.find_first_by_auth_conditions({ email: "example@example.com" }, id: user.id + 1), nil
+    user = User.create!(:email => "example@example.com", :password => "123456")
+    assert_equal User.find_first_by_auth_conditions({ :email => "example@example.com" }), user
+    assert_equal User.find_first_by_auth_conditions({ :email => "example@example.com" }, :id => user.id + 1), nil
   end
 end

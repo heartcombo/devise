f3928d8462ea4ebf68fa1e3903fe83d95b7ef35b
Ryan Mohr <ryan.mohr@gmail.com>
2013-02-25 10:42:06 -1000

    Mention devise-encryptable requirement for anything but bcrypt.

--- lib/generators/templates/devise.rb
+++ lib/generators/templates/devise.rb
@@ -175,7 +175,9 @@ Devise.setup do |config|
   # :sha1, :sha512 or encryptors from others authentication tools as :clearance_sha1,
   # :authlogic_sha512 (then you should set stretches above to 20 for default behavior)
   # and :restful_authentication_sha1 (then you should set stretches to 10, and copy
-  # REST_AUTH_SITE_KEY to pepper)
+  # REST_AUTH_SITE_KEY to pepper).
+  #
+  # Require the `devise-encryptable` gem when using anything other than bcrypt
   # config.encryptor = :sha512
 
   # ==> Configuration for :token_authenticatable

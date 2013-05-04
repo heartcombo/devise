a0058d5fbff8192e31ac7e1cc41ac4f07e452f9d
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2013-05-03 21:52:00 -0300

    Add some spaces, remove comment

--- test/controllers/passwords_controller_test.rb
+++ test/controllers/passwords_controller_test.rb
@@ -6,12 +6,15 @@ class PasswordsControllerTest < ActionController::TestCase
 
   def setup
     request.env["devise.mapping"] = Devise.mappings[:user]
+
     @user = create_user
     @user.send_reset_password_instructions
   end
 
   def put_update_with_params
-    put :update, "user"=>{"reset_password_token"=>@user.reset_password_token, "password"=>"123456", "password_confirmation"=>"123456"}
+    put :update, "user" => {
+      "reset_password_token" => @user.reset_password_token, "password" => "123456", "password_confirmation" => "123456"
+    }
   end
 
   test 'redirect to after_sign_in_path_for if after_resetting_password_path_for is not overridden' do
@@ -21,8 +24,8 @@ class PasswordsControllerTest < ActionController::TestCase
 
   test 'redirect accordingly if after_resetting_password_path_for is overridden' do
     custom_path = "http://custom.path/"
-    # Overwrite after_resetting_password_path_for with custom_path
     Devise::PasswordsController.any_instance.stubs(:after_resetting_password_path_for).with(@user).returns(custom_path)
+
     put_update_with_params
     assert_redirected_to custom_path
   end

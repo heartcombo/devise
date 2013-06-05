6fe74527d491280e1c861b69fe7965f34aa039f5
Ilya Vassilevsky <vassilevsky@gmail.com>
2013-06-05 15:42:39 +0400

    Logging authentication via the DatabaseAuthenticatable strategy

--- lib/devise/strategies/database_authenticatable.rb
+++ lib/devise/strategies/database_authenticatable.rb
@@ -5,12 +5,16 @@ module Devise
     # Default strategy for signing in a user, based on his email and password in the database.
     class DatabaseAuthenticatable < Authenticatable
       def authenticate!
+        logger.debug("Performing authentication with the DatabaseAuthenticatable strategy...")
         resource = valid_password? && mapping.to.find_for_database_authentication(authentication_hash)
 
         if validate(resource){ resource.valid_password?(password) }
+          logger.debug("DatabaseAuthenticatable strategy validated the password successfully.")
           resource.after_database_authentication
+          logger.debug("Authentication with the DatabaseAuthenticatable strategy succeeded.")
           success!(resource)
         elsif !halted?
+          logger.debug("Authentication with the DatabaseAuthenticatable strategy failed.")
           fail(:invalid)
         end
       end

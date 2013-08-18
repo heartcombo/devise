cd982123a819d02204cb7a10b7fcdab43fd530bd
José Valim <jose.valim@plataformatec.com.br>
2013-08-18 10:29:38 +0200

    Release 2.2.7

--- lib/devise/models/confirmable.rb
+++ lib/devise/models/confirmable.rb
@@ -215,11 +215,6 @@ module Devise
           generate_confirmation_token && save(:validate => false)
         end
 
-        def after_password_reset
-          super
-          confirm! unless confirmed?
-        end
-
         def postpone_email_change_until_confirmation
           @reconfirmation_required = true
           self.unconfirmed_email = self.email
--- lib/devise/version.rb
+++ lib/devise/version.rb
@@ -1,3 +1,3 @@
 module Devise
-  VERSION = "2.2.6".freeze
+  VERSION = "2.2.7".freeze
 end

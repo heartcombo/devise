b9112d4308115bd1c0c7b49969381d1016f5fb8b
Alexander Zubkov <info@zubkov.info>
2013-09-02 14:48:19 +0400

    Handle nil failed_attempts

--- lib/devise/hooks/lockable.rb
+++ lib/devise/hooks/lockable.rb
@@ -2,6 +2,6 @@
 # This is only triggered when the user is explicitly set (with set_user)
 Warden::Manager.after_set_user :except => :fetch do |record, warden, options|
   if record.respond_to?(:failed_attempts) && warden.authenticated?(options[:scope])
-    record.update_attribute(:failed_attempts, 0) unless record.failed_attempts.zero?
+    record.update_attribute(:failed_attempts, 0) unless record.failed_attempts.to_i.zero?
   end
 end

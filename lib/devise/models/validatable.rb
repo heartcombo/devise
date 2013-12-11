41833189db8b4501232a6922df634e1d9f6f08ae
Aayush khandelwal <aayush.khandelwal@vinsol.com>
2013-12-11 16:02:36 +0530

    Adding option in devise to check case sensitivity for email validation

--- lib/devise/models/validatable.rb
+++ lib/devise/models/validatable.rb
@@ -27,7 +27,7 @@ module Devise
 
         base.class_eval do
           validates_presence_of   :email, :if => :email_required?
-          validates_uniqueness_of :email, :allow_blank => true, :if => :email_changed?
+          validates_uniqueness_of :email, :allow_blank => true, :case_sensitive => Devise.email_case_sensitive ,:if => :email_changed?
           validates_format_of     :email, :with  => email_regexp, :allow_blank => true, :if => :email_changed?
 
           validates_presence_of     :password, :if => :password_required?

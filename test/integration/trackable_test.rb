49086d0294d0b2e18ebb8d7a83aa15063d8e703c
Vasiliy Ermolovich <younash@gmail.com>
2014-01-04 18:54:19 +0300

    remove warnings regarding number extensions

--- test/integration/trackable_test.rb
+++ test/integration/trackable_test.rb
@@ -63,7 +63,7 @@ class TrackableHooksTest < ActionDispatch::IntegrationTest
   end
 
   test "does not update anything if user has signed out along the way" do
-    swap Devise, :allow_unconfirmed_access_for => 0 do
+    swap Devise, :allow_unconfirmed_access_for => 0.days do
       user = create_user(:confirm => false)
       sign_in_as_user
 

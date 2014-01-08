a20b1146d9ac08f0e5e1fe53dd89e834519ec991
Rimian Perkins <hello@rimian.com.au>
2014-01-09 10:10:25 +1100

    name space methods

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -10,21 +10,21 @@ module DeviseHelper
 
     html = <<-HTML
     <div id="error_explanation">
-      <h2>#{sentence}</h2>
-      <ul>#{messages}</ul>
+      <h2>#{devise_error_sentence}</h2>
+      <ul>#{devise_error_message}</ul>
     </div>
     HTML
 
     html.html_safe
   end
 
-  def sentence
+  def devise_error_sentence
     I18n.t("errors.messages.not_saved",
                           :count => resource.errors.count,
                           :resource => resource.class.model_name.human.downcase)
   end
 
-  def messages(tag = :li)
+  def devise_error_message(tag = :li)
     resource.errors.full_messages.map { |msg| content_tag(tag, msg) }.join()
   end
 end

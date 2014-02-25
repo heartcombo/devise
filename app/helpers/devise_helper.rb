470032d68f026c39c79e9270f60552d1d833b1dc
Anshul Sharma <justanshulsharma@gmail.com>
2014-02-25 21:53:45 +0530

    Updated ruby 1.9 hash syntax

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -10,8 +10,8 @@ module DeviseHelper
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
     sentence = I18n.t("errors.messages.not_saved",
-                      :count => resource.errors.count,
-                      :resource => resource.class.model_name.human.downcase)
+                      count: resource.errors.count,
+                      resource: resource.class.model_name.human.downcase)
 
     html = <<-HTML
     <div id="error_explanation">

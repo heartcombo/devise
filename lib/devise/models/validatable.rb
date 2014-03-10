dd27defd13c373098bb7e6a5fd154610f36747f0
Simão Mata <me@simao.io>
2014-03-10 18:30:28 +0100

    Use `case_sensitive: false` when validating uniquess using ActiveRecord

--- lib/devise/models/validatable.rb
+++ lib/devise/models/validatable.rb
@@ -27,7 +27,7 @@ module Devise
 
         base.class_eval do
           validates_presence_of   :email, if: :email_required?
-          validates_uniqueness_of :email, allow_blank: true, if: :email_changed?
+          validates_uniqueness_of :email, allow_blank: true, case_sensitive: false, if: :email_changed?
           validates_format_of     :email, with: email_regexp, allow_blank: true, if: :email_changed?
 
           validates_presence_of     :password, if: :password_required?

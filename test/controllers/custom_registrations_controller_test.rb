90e55409e115bd82f55eeaecff50b688ff2d69f6
Daniel Pehrson <dpehrson@qurve.com>
2014-03-21 15:24:25 -0400

    Upgrade stray legacy hash syntax to 1.9 syntax.

--- test/controllers/custom_registrations_controller_test.rb
+++ test/controllers/custom_registrations_controller_test.rb
@@ -12,24 +12,24 @@ class CustomRegistrationsControllerTest < ActionController::TestCase
   end
 
   test "yield resource to block on create success" do
-    post :create, {user: {:email => "user@example.org", :password => "password", :password_confirmation => "password"}}
+    post :create, { user: { email: "user@example.org", password: "password", password_confirmation: "password" } }
     assert @controller.create_block_called?, "create failed to yield resource to provided block"
   end
 
   test "yield resource to block on create failure" do
-    post :create, {user: {}}
+    post :create, { user: { } }
     assert @controller.create_block_called?, "create failed to yield resource to provided block"
   end
 
   test "yield resource to block on update success" do
     sign_in @user
-    put :update, {user: {current_password: @password}}
+    put :update, { user: { current_password: @password } }
     assert @controller.update_block_called?, "update failed to yield resource to provided block"
   end
 
   test "yield resource to block on update failure" do
     sign_in @user
-    put :update, {user: {}}
+    put :update, { user: { } }
     assert @controller.update_block_called?, "update failed to yield resource to provided block"
   end
 end

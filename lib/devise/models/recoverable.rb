d6cd7ff461936d7d2b44b5885094c273e48aa3ba
Kenny Meyer <knny.myer@gmail.com>
2014-06-12 18:10:55 -0400

    Customization

--- lib/devise/models/recoverable.rb
+++ lib/devise/models/recoverable.rb
@@ -108,9 +108,8 @@ module Devise
         # Attributes must contain reset_password_token, password and confirmation
         def reset_password_by_token(attributes={})
           original_token       = attributes[:reset_password_token]
-          reset_password_token = Devise.token_generator.digest(self, :reset_password_token, original_token)
 
-          recoverable = find_or_initialize_with_error_by(:reset_password_token, reset_password_token)
+          recoverable = find_or_initialize_with_error_by(:reset_password_token, original_token)
 
           if recoverable.persisted?
             if recoverable.reset_password_period_valid?

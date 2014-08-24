d8d9814ed4889e6bbe7fd2ee9e4b5a6823f1e0db
Chun-Yang <yang2007chun@gmail.com>
2014-08-23 23:29:45 -0500

    user erb template to generate controllers, enable  --controllers option to specify controllers, make 'scope' argument required

--- lib/generators/devise/controllers_generator.rb
+++ lib/generators/devise/controllers_generator.rb
@@ -3,13 +3,23 @@ require 'rails/generators/base'
 module Devise
   module Generators
     class ControllersGenerator < Rails::Generators::Base
+      def self.all_controllers
+        @@controllers ||= %w(confirmations passwords registrations sessions unlocks omniauth_callbacks)
+      end
+
       desc "Create inherited Devise controllers in your application."
-      source_root File.expand_path("../../templates/", __FILE__)
-      argument :scope, required: false, default: nil,
-                       desc: "The scope to create controllers in"
+      source_root File.expand_path("../../templates/controllers", __FILE__)
+      argument :scope, required: true,
+                       desc: "The scope to create controllers in, e.g. users, admins"
+      class_option :controllers, aliases: "-c", type: :array, desc: "Select specific controllers to generate (#{all_controllers})"
 
       def create_controllers
-        directory "controllers", "app/controllers/#{scope || :devise}"
+        @scope_module = scope.camelize
+        controllers = options[:controllers] || self.class.all_controllers
+        controllers.each do |name|
+          template "#{name}_controller.erb",
+                   "app/controllers/#{scope}/#{name}_controller.rb"
+        end
       end
     end
   end

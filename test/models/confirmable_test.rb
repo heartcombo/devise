c1864663aa913ad1299cbc8503592259bdb07322
James Kiesel <james.kiesel@gmail.com>
2014-10-21 02:28:58 +1300

    Localize confirmable tests

--- test/models/confirmable_test.rb
+++ test/models/confirmable_test.rb
@@ -219,14 +219,14 @@ class ConfirmableTest < ActiveSupport::TestCase
   test 'should not be active when confirm in is zero' do
     Devise.allow_unconfirmed_access_for = 0.days
     user = create_user
-    user.confirmation_sent_at = Date.today
+    user.confirmation_sent_at = Time.zone.today
     assert_not user.active_for_authentication?
   end
 
   test 'should be active when we set allow_unconfirmed_access_for to nil' do
     swap Devise, allow_unconfirmed_access_for: nil do
       user = create_user
-      user.confirmation_sent_at = Date.today
+      user.confirmation_sent_at = Time.zone.today
       assert user.active_for_authentication?
     end
   end

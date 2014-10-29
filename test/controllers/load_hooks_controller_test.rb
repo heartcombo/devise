e46a21be16b9c96b08682183b2af72323d570e55
Chaker Nakhli <chaker.nakhli@sinbadsoft.com>
2014-10-29 16:14:38 +0100

    remove dynamically added method to DeviseController by active support load hook test

--- test/controllers/load_hooks_controller_test.rb
+++ test/controllers/load_hooks_controller_test.rb
@@ -3,10 +3,14 @@ require 'test_helper'
 class LoadHooksControllerTest < ActionController::TestCase
   setup do
     ActiveSupport.on_load(:devise_controller) do
-      define_method  :defined_by_load_hook do
+      define_method :defined_by_load_hook do
         puts 'I am defined dynamically by activesupport load hook'
       end
     end
+  end
+
+  teardown do
+    DeviseController.class_eval { undef :defined_by_load_hook }
   end
 
   test 'load hook called when controller is loaded' do

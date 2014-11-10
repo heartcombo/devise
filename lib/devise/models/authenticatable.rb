9b19d39439371f893f7875df76c68081f356f56f
BM5k <me@bm5k.com>
2014-11-09 21:51:40 -0700

    instrument Authenticatable#send_devise_notification
    The notification only works if `send_devise_notification` isn't overriden.

--- lib/devise/models/authenticatable.rb
+++ lib/devise/models/authenticatable.rb
@@ -185,12 +185,14 @@ module Devise
       #     end
       #
       def send_devise_notification(notification, *args)
-        message = devise_mailer.send(notification, self, *args)
-        # Remove once we move to Rails 4.2+ only.
-        if message.respond_to?(:deliver_now)
-          message.deliver_now
-        else
-          message.deliver
+        instrument "send.#{notification}.notification.devise", default_instrument_payload do
+          message = devise_mailer.send(notification, self, *args)
+          # Remove once we move to Rails 4.2+ only.
+          if message.respond_to?(:deliver_now)
+            message.deliver_now
+          else
+            message.deliver
+          end
         end
       end
 

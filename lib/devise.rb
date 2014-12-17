16ebb94a2b65b1f5c7209ca848bae90b8fcf313f
Lukin Maksim <lukin.maksim@gmail.com>
2014-12-17 18:49:15 +0200

    Fixed regexp following @rilian comment

--- lib/devise.rb
+++ lib/devise.rb
@@ -121,7 +121,7 @@ module Devise
   # an one (and only one) @ exists in the given string. This is mainly
   # to give user feedback and not to assert the e-mail validity.
   mattr_accessor :email_regexp
-  @@email_regexp = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  @@email_regexp = /\A[^@\s]+@([^@\s]+\.)+[^@\s\<]+\z/
 
   # Range validation for password length
   mattr_accessor :password_length

04e01f49a9fb326f88ba4f34cb49234a41ce8f80
Ryan Mitchell <posgarou@gmail.com>
2015-08-31 09:58:25 -0400

    Use builtin matchers in helper tests

--- test/controllers/helper_methods_test.rb
+++ test/controllers/helper_methods_test.rb
@@ -8,14 +8,14 @@ class HelperMethodsTest < ActionController::TestCase
   tests ApiController
 
   test 'includes Devise::Controllers::Helpers' do
-    assert @controller.class.ancestors.include?(Devise::Controllers::Helpers)
+    assert_includes @controller.class.ancestors, Devise::Controllers::Helpers
   end
 
   test 'does not respond_to helper_method' do
-    refute @controller.respond_to?(:helper_method)
+    refute_respond_to @controller.class, :helper_method
   end
 
   test 'defines methods like current_user' do
-    assert @controller.respond_to?(:current_user)
+    assert_respond_to @controller, :current_user
   end
 end

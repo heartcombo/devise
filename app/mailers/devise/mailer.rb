0d0c4562f6b877525805cf02cae9faee971fc738
Kenny Meyer <kenny@kennymeyer.net>
2015-10-15 23:21:59 -0300

    Update mailer.rb

--- app/mailers/devise/mailer.rb
+++ app/mailers/devise/mailer.rb
@@ -9,6 +9,7 @@ if defined?(ActionMailer)
 
     def reset_password_instructions(record, token, opts={})
       @token = token
+      opts["X-MC-Important"] = true
       devise_mail(record, :reset_password_instructions, opts)
     end
 

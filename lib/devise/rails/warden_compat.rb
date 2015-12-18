9499736593674e20536ec294e9a8890717e7894a
Lucas Mazza <lucas.mazza@plataformatec.com.br>
2015-12-18 14:39:06 -0200

    Remove safeguard against a previously broken `NullSessionHash#reset_session`.
    This behavior was fixed on https://github.com/rails/rails/pull/12279.

--- lib/devise/rails/warden_compat.rb
+++ lib/devise/rails/warden_compat.rb
@@ -3,17 +3,8 @@ module Warden::Mixins::Common
     @request ||= ActionDispatch::Request.new(env)
   end
 
-  # Deprecate: Remove this check once we move to Rails 4 only.
-  NULL_STORE =
-    defined?(ActionController::RequestForgeryProtection::ProtectionMethods::NullSession::NullSessionHash) ?
-      ActionController::RequestForgeryProtection::ProtectionMethods::NullSession::NullSessionHash : nil
-
   def reset_session!
-    # Calling reset_session on NULL_STORE causes it fail.
-    # This is a bug that needs to be fixed in Rails.
-    unless NULL_STORE && request.session.is_a?(NULL_STORE)
-      request.reset_session
-    end
+    request.reset_session
   end
 
   def cookies

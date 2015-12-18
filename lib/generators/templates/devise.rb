4e45a5fde4d069ffa53b833d477edd3cc7259c2e
Lucas Mazza <lucas.mazza@plataformatec.com.br>
2015-12-18 14:45:28 -0200

    Remove Rails version mention from the initializer template.

--- lib/generators/templates/devise.rb
+++ lib/generators/templates/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  # Devise will use the `secret_key_base` on Rails 4+ applications as its `secret_key`
+  # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '<%= SecureRandom.hex(64) %>'
 

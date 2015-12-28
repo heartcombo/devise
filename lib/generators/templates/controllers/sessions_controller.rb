924a989f377214afedf83fb64bd39260c8acd623
James Darling <james@abscond.org>
2015-12-28 00:45:03 +0000

    Update controller generator templates for new ParameterSanitizer syntax
    Devise::ParameterSanitizer has a new syntax for permitting additional
    attributes to a model. This commit updates the generated controllers to
    reflect that.

--- lib/generators/templates/controllers/sessions_controller.rb
+++ lib/generators/templates/controllers/sessions_controller.rb
@@ -20,6 +20,6 @@ class <%= @scope_prefix %>SessionsController < Devise::SessionsController
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
-  #   devise_parameter_sanitizer.for(:sign_in) << :attribute
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 end

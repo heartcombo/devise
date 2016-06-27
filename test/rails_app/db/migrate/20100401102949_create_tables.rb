22784cb55abefc8a62c69dcc170a30d18e45c7ac
Lucas Mazza <lucas.mazza@plataformatec.com.br>
2016-06-27 16:50:26 -0300

    Remove `ActiveRecord::Migration` inheritance deprecation warning.

--- test/rails_app/db/migrate/20100401102949_create_tables.rb
+++ test/rails_app/db/migrate/20100401102949_create_tables.rb
@@ -1,4 +1,8 @@
-class CreateTables < ActiveRecord::Migration
+superclass = ActiveRecord::Migration
+# TODO: Inherit from the 5.0 Migration class directly when we drop support for Rails 4.
+superclass = ActiveRecord::Migration[5.0] if superclass.respond_to?(:[])
+
+class CreateTables < superclass
   def self.up
     create_table :users do |t|
       t.string :username

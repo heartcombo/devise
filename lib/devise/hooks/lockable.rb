e841c4c5ef826b6838b269d259170bb4fbca82f1
Nicolas Viennot <nicolas@viennot.biz>
2016-07-13 10:13:14 -0400

    Reduce ORM API surface

--- lib/devise/hooks/lockable.rb
+++ lib/devise/hooks/lockable.rb
@@ -2,6 +2,9 @@
 # This is only triggered when the user is explicitly set (with set_user)
 Warden::Manager.after_set_user except: :fetch do |record, warden, options|
   if record.respond_to?(:failed_attempts) && warden.authenticated?(options[:scope])
-    record.update_attribute(:failed_attempts, 0) unless record.failed_attempts.to_i.zero?
+    unless record.failed_attempts.to_i.zero?
+      record.failed_attempts = 0
+      record.save(validate: false)
+    end
   end
 end

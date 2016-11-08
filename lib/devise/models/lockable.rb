434f411d43df683e911371c1b4e24cacaf48af42
Lutz Lengemann <mobilutz@users.noreply.github.com>
2016-11-08 12:19:15 +0100

    Move failed_attempts increment into its a function
    With this change one can overwrite when the incrementation is handled without duplicating the other `valid_for_authentication` logic.

--- lib/devise/models/lockable.rb
+++ lib/devise/models/lockable.rb
@@ -99,8 +99,7 @@ module Devise
         if super && !access_locked?
           true
         else
-          self.failed_attempts ||= 0
-          self.failed_attempts += 1
+          increment_failed_attempts
           if attempts_exceeded?
             lock_access! unless access_locked?
           else
@@ -108,6 +107,11 @@ module Devise
           end
           false
         end
+      end
+      
+      def increment_failed_attempts
+        self.failed_attempts ||= 0
+        self.failed_attempts += 1
       end
 
       def unauthenticated_message

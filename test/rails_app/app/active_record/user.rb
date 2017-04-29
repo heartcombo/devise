be2e17841e6c9fba1ae255f447803b8337ca6354
Dan Schultzer <dan@dreamconception.com>
2017-04-29 10:24:22 -0700

    Separate Devise test methods from Devise

--- test/rails_app/app/active_record/user.rb
+++ test/rails_app/app/active_record/user.rb
@@ -3,5 +3,5 @@ require 'shared_user'
 class User < ActiveRecord::Base
   include Shim
   include SharedUser
-  include ActiveModel::Serializers::Xml if Devise.rails5?
+  include ActiveModel::Serializers::Xml if Devise::Test.rails5?
 end
--- test/rails_app/app/controllers/users/omniauth_callbacks_controller.rb
+++ test/rails_app/app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,6 +9,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user = User.to_adapter.find_first(email: 'user@test.com')
     user.remember_me = true
     sign_in user
-    render (Devise.rails5? ? :body : :text) => ""
+    render (Devise::Test.rails5? ? :body : :text) => ""
   end
 end

heartcombo/devise

commit bb44d422d6442fb77904f7fd65146f3460c12f30

not signed_in 302 redirect failed on Rails5 RC2

lib/devise/test/controller_helpers.rb
@@ -137,9 +137,8 @@ module Devise
 
           status, headers, response = Devise.warden_config[:failure_app].call(env).to_a
           @controller.response.headers.merge!(headers)
-          r_opts = { status: status, content_type: headers["Content-Type"], location: headers["Location"] }
-          r_opts[Rails.version.start_with?('5') ? :body : :text] = response.body
-          @controller.send :render, r_opts
+          @controller.status = status
+          @controller.response.body = response.body
           nil # causes process return @response
         end
 

commit 3e23371b01443d6b8bdbfcbdcd33d2883847e39d

Preserve content_type for unauthenticated tests
In Rails 4, for unauthenticated controller tests which trigger the
failure app, ensure that the simulated failure response includes a
content_type (broken in bb44d42).

This works in Rails5, which parses the content-type header on-demand,
but not in Rails4 which requires setting the response's content_type
explicitly.

Fixes #4783.

lib/devise/test/controller_helpers.rb
@@ -139,6 +139,7 @@ module Devise
 
           status, headers, response = Devise.warden_config[:failure_app].call(env).to_a
           @controller.response.headers.merge!(headers)
+          @controller.response.content_type = headers["Content-Type"] unless Devise::Test.rails5?
           @controller.status = status
           @controller.response.body = response.body
           nil # causes process return @response

test/test/controller_helpers_test.rb
@@ -100,6 +100,11 @@ class TestControllerHelpersTest < Devise::ControllerTestCase
     assert_equal response.body, "<html><body>You are being <a href=\"http://test.host/users/sign_in\">redirected</a>.</body></html>"
   end
 
+  test "returns the content type of a failure app" do
+    get :index, params: { format: :xml }
+    assert response.content_type.include?('application/xml')
+  end
+
   test "defined Warden after_authentication callback should not be called when sign_in is called" do
     begin
       Warden::Manager.after_authentication do |user, auth, opts|
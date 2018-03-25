e44a8b2d434dd078bfd4bd38849504e9a6cfc169
Wolfram Nikolas Müller <muellerwolfram@gmail.com>
2018-03-25 13:37:42 +0200

    Include configuration for Turbolinks in devise initializer template

--- lib/generators/templates/devise.rb
+++ lib/generators/templates/devise.rb
@@ -280,4 +280,11 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
+  # ==> Turbolinks configuration
+  # If your app is using Turbolinks, Turbolinks::Controller needs to be included to make redirection work correctly:
+  #
+  # ActiveSupport.on_load(:devise_failure_app)
+  #   include Turbolinks::Controller
+  # end
 end

2bb8e1c236c068f7726ff3aaef7e9106e9a9a9c5
Wolfram Nikolas Müller <muellerwolfram@gmail.com>
2018-03-27 16:32:06 +0200

    Fix missing do block in config

--- lib/generators/templates/devise.rb
+++ lib/generators/templates/devise.rb
@@ -284,7 +284,7 @@ Devise.setup do |config|
   # ==> Turbolinks configuration
   # If your app is using Turbolinks, Turbolinks::Controller needs to be included to make redirection work correctly:
   #
-  # ActiveSupport.on_load(:devise_failure_app)
+  # ActiveSupport.on_load(:devise_failure_app) do
   #   include Turbolinks::Controller
   # end
 end

af8f7e9797ad903d30e471ca04e6194816248094
Samy Kacimi <samy.kacimi@protonmail.ch>
2018-05-14 23:11:15 +0200

    remove :trackable from the default modules (#4857)
    This removes :trackable from the default modules in the generators,
    to be more GDPR-friendly.

--- lib/generators/active_record/devise_generator.rb
+++ lib/generators/active_record/devise_generator.rb
@@ -54,11 +54,11 @@ module ActiveRecord
       t.datetime :remember_created_at
 
       ## Trackable
-      t.integer  :sign_in_count, default: 0, null: false
-      t.datetime :current_sign_in_at
-      t.datetime :last_sign_in_at
-      t.#{ip_column} :current_sign_in_ip
-      t.#{ip_column} :last_sign_in_ip
+      # t.integer  :sign_in_count, default: 0, null: false
+      # t.datetime :current_sign_in_at
+      # t.datetime :last_sign_in_at
+      # t.#{ip_column} :current_sign_in_ip
+      # t.#{ip_column} :last_sign_in_ip
 
       ## Confirmable
       # t.string   :confirmation_token

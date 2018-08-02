acc45c5a44c45b252ccba65fd169a45af73ff369
Isaac Orme <isorme1@gmail.com>
2018-08-02 07:02:50 -0700

    issue 4356 adds is_navigational_format? check to after_sign_up_path_for (#4833)

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -112,7 +112,7 @@ class Devise::RegistrationsController < DeviseController
   # The path used after sign up. You need to overwrite this method
   # in your own RegistrationsController.
   def after_sign_up_path_for(resource)
-    after_sign_in_path_for(resource)
+    after_sign_in_path_for(resource) if is_navigational_format?
   end
 
   # The path used after sign up for inactive accounts. You need to overwrite

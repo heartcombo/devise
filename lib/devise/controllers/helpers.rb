70e460a6998248452cecb1bf0fe6aa68fa2459e5
Maarten Brouwers <github.com@murb.nl>
2018-09-28 09:57:53 +0200

    pop stored_location on after_sign_in_path

--- lib/devise/controllers/helpers.rb
+++ lib/devise/controllers/helpers.rb
@@ -213,7 +213,7 @@ module Devise
       #   end
       #
       def after_sign_in_path_for(resource_or_scope)
-        stored_location_for(resource_or_scope) || signed_in_root_path(resource_or_scope)
+        (is_navigational_format? ? pop_stored_location_for(resource_or_scope) : stored_location_for(resource_or_scope)) || signed_in_root_path(resource_or_scope)
       end
 
       # Method used by sessions controller to sign out a user. You can overwrite

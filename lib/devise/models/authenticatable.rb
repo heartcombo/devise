c8ccacc32f11fdb91582d3ca53776dd09c95d53f
Alex J <2790993+dalexj@users.noreply.github.com>
2018-10-18 12:27:45 -0600

    add in a pretty_print method for better pry output

--- lib/devise/models/authenticatable.rb
+++ lib/devise/models/authenticatable.rb
@@ -125,6 +125,27 @@ module Devise
         "#<#{self.class} #{inspection.join(", ")}>"
       end
 
+      def pretty_print(pp)
+        pp.object_address_group(self) do
+          if defined?(@attributes) && @attributes
+            columns = serializable_hash.select { |column_name, column_value| has_attribute?(column_name) || new_record? }
+            pp.seplist(columns, proc { pp.text ',' }) do |column_name, column_value|
+              column_value = read_attribute(column_name)
+              pp.breakable ' '
+              pp.group(1) do
+                pp.text column_name
+                pp.text ':'
+                pp.breakable
+                pp.pp column_value
+              end
+            end
+          else
+            pp.breakable ' '
+            pp.text 'not initialized'
+          end
+        end
+      end
+
       protected
 
       def devise_mailer

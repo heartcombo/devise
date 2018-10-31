c51a3596412976d761d18cf660965e63c9534413
joaumg <joaumg@gmail.com>
2018-10-31 02:44:06 -0300

    chore(docs): allow_unconfirmed_access_for = nil (#2275)

--- lib/generators/templates/devise.rb
+++ lib/generators/templates/devise.rb
@@ -126,8 +126,11 @@ Devise.setup do |config|
   # A period that the user is allowed to access the website even without
   # confirming their account. For instance, if set to 2.days, the user will be
   # able to access the website for two days without confirming their account,
-  # access will be blocked just in the third day. Default is 0.days, meaning
-  # the user cannot access the website without confirming their account.
+  # access will be blocked just in the third day.
+  # You can also set it to nil, which will allow the user to access the website
+  # without confirming their account.
+  # Default is 0.days, meaning the user cannot access the website without
+  # confirming their account.
   # config.allow_unconfirmed_access_for = 2.days
 
   # A period that the user is allowed to confirm their account before their

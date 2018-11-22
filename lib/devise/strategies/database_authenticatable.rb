5f62f28d6ec6b8b4799da3db770d2fa4ed4c5b8d
Leonardo Tegon <ltegon93@gmail.com>
2018-11-22 20:38:02 -0200

    Explain the code that prevents enumeration attacks

--- lib/devise/strategies/database_authenticatable.rb
+++ lib/devise/strategies/database_authenticatable.rb
@@ -16,6 +16,9 @@ module Devise
           success!(resource)
         end
 
+        # On paranoid mode, hash the password even when a resource doesn't exist for the given authentication key.
+        # This is necessary to prevent enumeration attacks - e.g. the request is faster when a resource doesn't
+        # exist in the database if the password hashing algorithm is not called.
         mapping.to.new.password = password if !hashed && Devise.paranoid
         unless resource
           Devise.paranoid ? fail(:invalid) : fail(:not_found_in_database)

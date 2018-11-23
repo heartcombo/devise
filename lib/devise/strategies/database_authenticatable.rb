52b24e41de5093fe60a89019b2462608874d5745
Leonardo Tegon <ltegon93@gmail.com>
2018-11-23 13:26:26 -0200

    Fix typo [ci skip]

--- lib/devise/strategies/database_authenticatable.rb
+++ lib/devise/strategies/database_authenticatable.rb
@@ -16,7 +16,7 @@ module Devise
           success!(resource)
         end
 
-        # On paranoid mode, hash the password even when a resource doesn't exist for the given authentication key.
+        # In paranoid mode, hash the password even when a resource doesn't exist for the given authentication key.
         # This is necessary to prevent enumeration attacks - e.g. the request is faster when a resource doesn't
         # exist in the database if the password hashing algorithm is not called.
         mapping.to.new.password = password if !hashed && Devise.paranoid

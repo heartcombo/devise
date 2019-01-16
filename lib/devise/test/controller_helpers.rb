58c0f2a3c816fb470e9eaf95d081d6df0ed2dc8a
Leonardo Tegon <ltegon93@gmail.com>
2019-01-16 10:44:16 +0000

    Condition on Rails major version instead of the version string
    Co-Authored-By: matthewrudy <MatthewRudyJacobs@gmail.com>

--- lib/devise/test/controller_helpers.rb
+++ lib/devise/test/controller_helpers.rb
@@ -139,7 +139,7 @@ module Devise
 
           status, headers, response = Devise.warden_config[:failure_app].call(env).to_a
           @controller.response.headers.merge!(headers)
-          @controller.response.content_type = headers["Content-Type"] unless Rails.version.start_with?('5', '6.0')
+          @controller.response.content_type = headers["Content-Type"] unless Rails::VERSION::MAJOR >= 5
           @controller.status = status
           @controller.response.body = response.body
           nil # causes process return @response

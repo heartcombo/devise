06b036c49e595e9380b7d9fbeb5bc2195e4cf173
Felipe Renan <felipe.renan@plataformatec.com.br>
2019-01-28 15:30:48 -0200

    Remove unnecessary checks for Rails 4

--- test/rails_app/config/application.rb
+++ test/rails_app/config/application.rb
@@ -33,12 +33,6 @@ module RailsApp
     # config.assets.enabled = false
 
     config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-    rails_version = Gem::Version.new(Rails.version)
-    if DEVISE_ORM == :active_record &&
-       rails_version >= Gem::Version.new('4.2.0') &&
-       rails_version < Gem::Version.new('5.1.0')
-      config.active_record.raise_in_transactional_callbacks = true
-    end
 
     # This was used to break devise in some situations
     config.to_prepare do

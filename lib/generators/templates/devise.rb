5d73e1e3bb4961e143ca3d6aa75ca13a7ef9a7cd
Ryan Lue <hello@ryanlue.com>
2019-09-25 11:02:20 +0800

    Explain layout of default config initializer [ci skip]

--- lib/generators/templates/devise.rb
+++ lib/generators/templates/devise.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+# Assuming you have not yet modified this file, each configuration option below
+# is set to its default value. Note that some are commented out while others
+# are not: uncommented lines are intended to protect your configuration from
+# breaking changes in upgrades (i.e., in the event that future versions of
+# Devise change the default values for those options).
+#
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|

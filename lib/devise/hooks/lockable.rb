8adea031f822dff079ea73a0b611b5ce3e8d3127
Austen Madden <amadden@ikesmartcity.com>
2020-05-20 16:32:51 -0400

    Skip lockable hook with skip_lockable set
    There are times where it it's not ideal to update the target record in
    this hook. Introducing this option allows a developer to choose not to
    update the record if they so desire.

--- lib/devise/hooks/lockable.rb
+++ lib/devise/hooks/lockable.rb
@@ -3,7 +3,10 @@
 # After each sign in, if resource responds to failed_attempts, sets it to 0
 # This is only triggered when the user is explicitly set (with set_user)
 Warden::Manager.after_set_user except: :fetch do |record, warden, options|
-  if record.respond_to?(:failed_attempts) && warden.authenticated?(options[:scope])
+  if record.respond_to?(:failed_attempts) &&
+     warden.authenticated?(options[:scope]) &&
+     !warden.request.env['devise.skip_lockable']
+
     unless record.failed_attempts.to_i.zero?
       record.failed_attempts = 0
       record.save(validate: false)

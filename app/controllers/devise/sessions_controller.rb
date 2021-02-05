874f955772a59c67f2ee65010daf27b3d9a175df
Aaron Frase <afrase91@gmail.com>
2021-02-05 12:02:22 -0500

    Fix syntax error

--- app/controllers/devise/sessions_controller.rb
+++ app/controllers/devise/sessions_controller.rb
@@ -26,7 +26,7 @@ class Devise::SessionsController < DeviseController
   # DELETE /resource/sign_out
   def destroy
     signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-    set_flash_message!(:notice, :signed_out if signed_out)
+    set_flash_message!(:notice, :signed_out) if signed_out
     yield if block_given?
     respond_to_on_destroy
   end

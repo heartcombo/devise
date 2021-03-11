48d97582d16e79dc2c68a1295c7726b6908cb753
Alex Ghiculescu <alex@tanda.co>
2021-03-11 14:56:57 -0600

    run tests against rails main

--- test/rails_app/config/application.rb
+++ test/rails_app/config/application.rb
@@ -46,7 +46,7 @@ module RailsApp
     end
 
     # Remove the first check once Rails 5.0 support is removed.
-    if Devise::Test.rails52_and_up? && !Devise::Test.rails6?
+    if Devise::Test.rails52_and_up? && !Devise::Test.rails6_and_up?
       Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
     end
   end

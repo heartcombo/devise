c9d5d430358cf2c360a7678d0c815e0d1ec22f60
AhMohsen46 <ahmad.mohsen46@gmail.com>
2021-08-10 00:03:17 +0200

    use allocate

--- app/controllers/devise/sessions_controller.rb
+++ app/controllers/devise/sessions_controller.rb
@@ -8,7 +8,7 @@ class Devise::SessionsController < DeviseController
 
   # GET /resource/sign_in
   def new
-    self.resource = resource_class.new(sign_in_params)
+    self.resource = resource_class.allocate(sign_in_params)
     clean_up_passwords(resource)
     yield resource if block_given?
     respond_with(resource, serialize_options(resource))

31c5694a11069fa0812db7f50d6d3f2d627bf3c7
Joseph Chen <jchen@dogomedia.com>
2022-02-10 08:32:27 -0500

    Fix bug with Devise.sign_out_all_scopes trying to edit resource after it has already been destroyed.

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -63,8 +63,8 @@ class Devise::RegistrationsController < DeviseController
 
   # DELETE /resource
   def destroy
-    resource.destroy
     Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+    resource.destroy
     set_flash_message! :notice, :destroyed
     yield resource if block_given?
     respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }

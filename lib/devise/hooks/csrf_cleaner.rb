b5172a0cdbc02686ae905d65ec2392ead41be139
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2022-04-22 10:41:01 -0300

    Fix csrf cleanup for Rails 7.1 (main)
    Rails implemented a CSRF token storage strategy to allow storing the
    CSRF tokens outside of the sessios (for example, in an encrypted
    cookie), and changed how the value is kept around during the request
    cycle, by using a request.env value.
    
    We still want to ensure the final session value is cleaned correctly in
    the test, but the implementation needed to change since we can't simply
    delete from the session anymore, we need to make sure we call the Rails
    methods for resetting the current storage strategy so it works with all
    of them.
    
    https://github.com/rails/rails/pull/44283

--- lib/devise/hooks/csrf_cleaner.rb
+++ lib/devise/hooks/csrf_cleaner.rb
@@ -4,6 +4,12 @@ Warden::Manager.after_authentication do |record, warden, options|
   clean_up_for_winning_strategy = !warden.winning_strategy.respond_to?(:clean_up_csrf?) ||
     warden.winning_strategy.clean_up_csrf?
   if Devise.clean_up_csrf_token_on_authentication && clean_up_for_winning_strategy
-    warden.request.session.try(:delete, :_csrf_token)
+    request = warden.request
+    if request.respond_to?(:controller_instance) && request.controller_instance.respond_to?(:reset_csrf_token)
+      # Rails 7.1+
+      request.controller_instance.reset_csrf_token(request)
+    else
+      request.session.try(:delete, :_csrf_token)
+    end
   end
 end

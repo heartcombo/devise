2fa9303ab35f159d7c3fb76392692994155b18f0
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2022-04-22 12:55:08 -0300

    Use new method to reset CSRF exposed via the request object
    This simplifies the logic considerably, as we don't need to reach out to
    what seems more internal-ish implementation of Rails with the
    interaction between the request and controller objects.
    
    https://github.com/rails/rails/commit/b925880914fa3dca8c9cd0f8e88fb18fc8ec180b

--- lib/devise/hooks/csrf_cleaner.rb
+++ lib/devise/hooks/csrf_cleaner.rb
@@ -4,12 +4,11 @@ Warden::Manager.after_authentication do |record, warden, options|
   clean_up_for_winning_strategy = !warden.winning_strategy.respond_to?(:clean_up_csrf?) ||
     warden.winning_strategy.clean_up_csrf?
   if Devise.clean_up_csrf_token_on_authentication && clean_up_for_winning_strategy
-    request = warden.request
-    if request.respond_to?(:controller_instance) && request.controller_instance.respond_to?(:reset_csrf_token)
+    if warden.request.respond_to?(:reset_csrf_token)
       # Rails 7.1+
-      request.controller_instance.reset_csrf_token(request)
+      warden.request.reset_csrf_token
     else
-      request.session.try(:delete, :_csrf_token)
+      warden.request.session.try(:delete, :_csrf_token)
     end
   end
 end

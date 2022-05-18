66ec3e6cac1bb149c16f65ccd3ff0b6b7daf7610
Corey Gardner <coreygardner@coreys-mbp.myfiosgateway.com>
2022-05-18 16:14:34 -0400

    Rename methods and add comments explaining their usage

--- lib/devise/views/helpers.rb
+++ lib/devise/views/helpers.rb
@@ -1,14 +1,36 @@
 module Devise
   module Views
     module Helpers
+
+      # Generates helpers that do something based on mappings authentication status,
+      # thereby avoiding the use of repetitive if/else blocks
+      #
+      # Generated Methods => signed_in_user & signed_out_user
+      #
+      # signed_in_user do
+      #    <h1>Hello <%= @user.name %> </h1>
+      # end
+      #
+      # signed_out_user do
+      #   <%= link_to "Sign In", new_user_session_path %>
+      # end
+      #
+      # These methods can also be used at the controller level:
+      #
+      # def some_action
+      #   signed_in_user {render @users}
+      #   signed_out_user {redirect_to root_path}
+      # end
+    
+
       def self.define_helpers(mapping)
         mapping = mapping.name
         class_eval <<-METHODS, __FILE__, __LINE__ + 1
-          def signed_in_#{mapping}_content(&block)
+          def signed_in_#{mapping}(&block)
             block.call if #{mapping}_signed_in?
           end
 
-          def signed_out_mapping_content(&block)
+          def signed_out_#{mapping}(&block)
             block.call if #{mapping}_signed_out?
           end
         METHODS

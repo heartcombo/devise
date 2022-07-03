58008419f881b561ec8cc13521f847ccc5ec335a
yasir azgar <yasirazgar@gmail.com>
2022-07-03 22:33:57 +0530

    5505 - trigger setter instead of setting local var
    use self.resource in devise_controller#require_no_authentication so that the setter is triggered, and instance variable @user is populated.

--- app/controllers/devise_controller.rb
+++ app/controllers/devise_controller.rb
@@ -111,7 +111,7 @@ MESSAGE
       warden.authenticated?(resource_name)
     end
 
-    if authenticated && resource = warden.user(resource_name)
+    if authenticated && (self.resource = warden.user(resource_name))
       set_flash_message(:alert, 'already_authenticated', scope: 'devise.failure')
       redirect_to after_sign_in_path_for(resource)
     end

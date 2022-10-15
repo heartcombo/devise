ac226815e6936971203aba5cae3d92b2b30d031f
jackparsons93 <jackparsons93@gmail.com>
2022-10-15 17:03:17 -0500

    Update navigational formats to accept turbo_Stream

--- lib/generators/templates/devise.rb
+++ lib/generators/templates/devise.rb
@@ -263,7 +263,7 @@ Devise.setup do |config|
   # should add them to the navigational formats lists.
   #
   # The "*/*" below is required to match Internet Explorer requests.
-  # config.navigational_formats = ['*/*', :html]
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete

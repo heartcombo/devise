a2d7bb9fc229bd8093e3c7d5bdfdac255cc982bc
Anthony Ross <anthony.s.ross@gmail.com>
2023-12-31 15:07:10 -0800

    Remove Duplication
    This removes duplicate examples of adding extra permitted params.

--- lib/generators/templates/controllers/registrations_controller.rb
+++ lib/generators/templates/controllers/registrations_controller.rb
@@ -45,11 +45,6 @@ class <%= @scope_prefix %>RegistrationsController < Devise::RegistrationsControl
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
   # end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
   #   super(resource)

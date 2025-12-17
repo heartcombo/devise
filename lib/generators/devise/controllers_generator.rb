ba5cdfbf1921b74d8aac93baffff72630cf39a09
nkroker <nikhilq.75@gmail.com>
2025-12-17 09:45:06 +0530

    Performed corrections in generator and also corrected the message

--- lib/generators/devise/controllers_generator.rb
+++ lib/generators/devise/controllers_generator.rb
@@ -5,13 +5,13 @@ require 'rails/generators/base'
 module Devise
   module Generators
     class ControllersGenerator < Rails::Generators::Base
-      CONTROLLERS = %w(confirmations passwords registrations sessions unlocks omniauth_callbacks).freeze
+      CONTROLLERS = %w[confirmations passwords registrations sessions unlocks omniauth_callbacks].freeze
 
       desc <<-DESC.strip_heredoc
         Create inherited Devise controllers in your app/controllers folder.
 
         Use -c to specify which controller you want to overwrite.
-        If you do no specify a controller, all controllers will be created.
+        If you do not specify a controller, all controllers will be created.
         For example:
 
           rails generate devise:controllers users -c=sessions
@@ -23,14 +23,14 @@ module Devise
           end
       DESC
 
-      source_root File.expand_path("../../templates/controllers", __FILE__)
+      source_root File.expand_path('../../templates/controllers', __FILE__)
       argument :scope, required: true,
-        desc: "The scope to create controllers in, e.g. users, admins"
-      class_option :controllers, aliases: "-c", type: :array,
-        desc: "Select specific controllers to generate (#{CONTROLLERS.join(', ')})"
+                       desc: 'The scope to create controllers in, e.g. users, admins'
+      class_option :controllers, aliases: '-c', type: :array,
+                                 desc: "Select specific controllers to generate (#{CONTROLLERS.join(', ')})"
 
       def create_controllers
-        @scope_prefix = scope.blank? ? '' : (scope.camelize + '::')
+        @scope_prefix = scope.blank? ? '' : "#{scope.camelize}::"
         controllers = options[:controllers] || CONTROLLERS
         controllers.each do |name|
           template "#{name}_controller.rb",
@@ -39,7 +39,7 @@ module Devise
       end
 
       def show_readme
-        readme "README" if behavior == :invoke
+        readme 'README' if behavior == :invoke
       end
     end
   end

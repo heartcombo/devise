640052ad34e52c0f9538d40b7927804babb2b947
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2026-03-27 19:24:19 -0300

    feat: intercept `DatabaseAuthenticatable` and `PasswordsController` for 2FA

--- lib/devise/strategies/database_authenticatable.rb
+++ lib/devise/strategies/database_authenticatable.rb
@@ -11,9 +11,13 @@ module Devise
         hashed = false
 
         if validate(resource){ hashed = true; resource.valid_password?(password) }
-          remember_me(resource)
-          resource.after_database_authentication
-          success!(resource)
+          if resource.respond_to?(:two_factor_enabled?) && resource.two_factor_enabled?
+            initiate_two_factor_authentication!(resource)
+          else
+            remember_me(resource)
+            resource.after_database_authentication
+            success!(resource)
+          end
         end
 
         # In paranoid mode, hash the password even when a resource doesn't exist for the given authentication key.
@@ -23,6 +27,20 @@ module Devise
         unless resource
           Devise.paranoid ? fail(:invalid) : fail(:not_found_in_database)
         end
+      end
+
+      private
+
+      def initiate_two_factor_authentication!(resource)
+        session["devise.two_factor.resource_id"] = resource.id
+        session["devise.two_factor.remember_me"] = remember_me?
+        default_method = resource.enabled_two_factors.first
+        redirect!(new_two_factor_challenge_path(scope, default_method))
+      end
+
+      def new_two_factor_challenge_path(scope, method)
+        Rails.application.routes.url_helpers
+          .send(:"#{scope}_new_two_factor_#{method}_path")
       end
     end
   end
